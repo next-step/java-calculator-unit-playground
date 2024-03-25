04b26cb638d0ad455b738e002a7900afd3693348
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-03-25 22:10:27 +0900

    [이호석] 1단계 - 초간단 계산기 구현 (#1)
    * docs: 기능 요구사항 정의
    
    * feat: 사칙연산 계산기 구현
    
    * docs: 클래스별 기능 사항 추가

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public int add(final int leftOperand, final int rightOperand) {
+        return leftOperand + rightOperand;
+    }
+
+    public int subtract(final int leftOperand, final int rightOperand) {
+        return leftOperand - rightOperand;
+    }
+
+    public int multiply(final int leftOperand, final int rightOperand) {
+        return leftOperand * rightOperand;
+    }
+
+    public int divide(final int leftOperand, final int rightOperand) {
+        return leftOperand / rightOperand;
+    }
+}
