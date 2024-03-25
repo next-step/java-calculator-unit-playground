e563a03fd5bd79cd92ff0f83c5c899a5dbc11ee4
Mint <audwl03071@gmail.com>
2024-03-25 18:18:19 +0900

    feat: Implement basic arithmetic operations for Calculator

---
+++ src/main/java/org/duckstudy/calculator/core/Calculator.java
@@ -0,0 +1,19 @@
+package org.duckstudy.calculator.core;
+public class Calculator {
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+
+    public static int divide(int a, int b){
+        if (b==0) throw new IllegalArgumentException("b cannot be zero");
+        else return a / b;
+    }
+}
