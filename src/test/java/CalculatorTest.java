969d1e81f5002f5bfe39d6132367b5c4c4b33e4d
HiiWee <wpdlzhf159@gmail.com>
2024-03-27 00:49:58 +0900

    test: 초간단 계산기 검증 테스트 추가

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    Calculator calculator;
+
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 덧셈을 할 수 있다.")
+    void add() {
+        // given
+        int a = 2;
+        int b = 3;
+
+        // when
+        int addedResult = calculator.add(a, b);
+
+        // then
+        assertEquals(5, addedResult);
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 뺄셈을 할 수 있다.")
+    void subtract() {
+        // given
+        int a = 3;
+        int b = 2;
+
+        // when
+        int subtractedResult = calculator.subtract(a, b);
+
+        // then
+        assertEquals(1, subtractedResult);
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 곱셈을 할 수 있다.")
+    void multiply() {
+        // given
+        int a = 3;
+        int b = 2;
+
+        // when
+        int multipliedResult = calculator.multiply(a, b);
+
+        // then
+        assertEquals(6, multipliedResult);
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 나눗셈을 할 수 있다.")
+    void divide() {
+        // given
+        int a = 32;
+        int b = 2;
+
+        // when
+        int dividedResult = calculator.divide(a, b);
+
+        // then
+        assertEquals(16, dividedResult);
+    }
+}
