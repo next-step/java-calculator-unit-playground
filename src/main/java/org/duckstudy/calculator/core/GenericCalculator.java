c627a7c77fdb0ed6699f942ed6f3c08b16d9d13a
Mint <audwl03071@gmail.com>
2024-03-28 00:15:42 +0900

    comment: 주석 삭제

--- src/main/java/org/duckstudy/calculator/core/GenericCalculator.java
+++ src/main/java/org/duckstudy/calculator/core/GenericCalculator.java
@@ -3,11 +3,6 @@ package org.duckstudy.calculator.core;
 import java.math.BigDecimal;
 import java.util.function.Function;
 
-/*
-    제네릭 계산기 : 정수, 소수점 계산을 허용한다.
-    - BigDecimal 타입으로 계산한다.
-    - Function의 toType() 메서드로 결과값을 입력값의 타입(T)로 형변환한다.
- */
 public class GenericCalculator<T extends Number> implements Calculator<T> {
     private BigDecimal result;
     private Function<BigDecimal, T> toType;
@@ -45,15 +40,4 @@ public class GenericCalculator<T extends Number> implements Calculator<T> {
     public void reset() {
         result = BigDecimal.ZERO;
     }
-
-//    public static void main(String[] args){
-//        GenericCalculator<Integer> gc = new GenericCalculator<>(BigDecimal::intValue);
-//        Number result = gc.add(3);
-//        System.out.println(result);
-//
-//        GenericCalculator<Double> gc2 = new GenericCalculator<>(BigDecimal::doubleValue);
-//        gc2.add(3.4);
-//        Number result2 = gc2.add(4.4);
-//        System.out.println(result2);
-//    }
 }
