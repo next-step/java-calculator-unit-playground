3b05cb72735ac910d8f87f32a5510b6d3524202f
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-03-28 08:21:49 +0900

    2단계 - 초간단 계산기 테스트 구현 (#2)
    * docs: 2단계 기능 문서 업데이트
    
    * test: 초간단 계산기 검증 테스트 추가
    
    * refactor: 테스트 필드 private 접근제어자 추가
    
    * feat: 0으로 나눌때 구체적인 예외 메시지 추가
    
    * style: 인덴트 정렬

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -13,6 +13,9 @@ public class Calculator {
     }
 
     public int divide(final int leftOperand, final int rightOperand) {
+        if (rightOperand == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
         return leftOperand / rightOperand;
     }
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,86 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 덧셈을 할 수 있다.")
+    void add() {
+        // given
+        int a = 2;
+        int b = 3;
+
+        // when
+        int addedResult = calculator.add(a, b);
+
+        // then
+        assertEquals(5, addedResult);
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 뺄셈을 할 수 있다.")
+    void subtract() {
+        // given
+        int a = 3;
+        int b = 2;
+
+        // when
+        int subtractedResult = calculator.subtract(a, b);
+
+        // then
+        assertEquals(1, subtractedResult);
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 곱셈을 할 수 있다.")
+    void multiply() {
+        // given
+        int a = 3;
+        int b = 2;
+
+        // when
+        int multipliedResult = calculator.multiply(a, b);
+
+        // then
+        assertEquals(6, multipliedResult);
+    }
+
+    @Test
+    @DisplayName("두 개의 인자를 받아 나눗셈을 할 수 있다.")
+    void divide() {
+        // given
+        int a = 32;
+        int b = 2;
+
+        // when
+        int dividedResult = calculator.divide(a, b);
+
+        // then
+        assertEquals(16, dividedResult);
+    }
+
+    @Test
+    @DisplayName("우항의 값이 0이라면 나눌 수 없다.")
+    void canNot_divide_bvZero() {
+        // given
+        int a = 32;
+        int b = 0;
+
+        // when
+        String message = assertThrows(ArithmeticException.class, () -> calculator.divide(a, b)).getMessage();
+
+        // then
+        assertEquals(message, "0으로 나눌 수 없습니다.");
+    }
+}
