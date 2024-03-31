ae7c8f35307b091751c8e1456bc6627829ca084d
Seokju Hong <iamseokjuhong@gmail.com>
2024-04-01 00:11:30 +0900

    테스트의 설명들 구체화

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest {
 
     @Test
-    @DisplayName("덧셈")
+    @DisplayName("두 개의 정수를 더해본다")
     void add() {
         final int expected = 12;
         final int actual = new Calculator().add(8, 4);
@@ -15,7 +15,7 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("뺄셈")
+    @DisplayName("두 개의 정수를 빼본다")
     void subtract() {
         final int expected = 4;
         final int actual = new Calculator().subtract(8, 4);
@@ -24,7 +24,7 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("곱셈")
+    @DisplayName("두 개의 정수를 곱해본다")
     void multiply() {
         final int expected = 32;
         final int actual = new Calculator().multiply(8, 4);
@@ -33,7 +33,7 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("나눗셈")
+    @DisplayName("두 개의 정수를 나눠본다")
     void divide() {
         final int expected = 2;
         final int actual = new Calculator().divide(8, 4);
