669cf97a5185a5595b8f2b242195e57e1b722c9b
fanngineer <fanngineer@gmail.com>
2024-04-03 22:43:13 +0900

    리뷰 반영 및 inner class 삭제
    빌드 문제로 inner class 삭제

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,27 +1,32 @@
+
 public class Calculator {
     public static int add(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-        return (int) (i + j);
+        if(i + j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
+        return (int) Math.round(i + j);
     }
 
     public static int subtract(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-        return (int) (i - j);
+        if(i - j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
+        return (int) Math.round(i - j);
     }
 
     public static int multiply(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-        return (int) (i * j);
+        if(i * j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
+        return (int) Math.round(i * j);
     }
 
     public static int divide(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-//        if(j==0) throw new IllegalArgumentException("[ERROR] 0으로 나눌 수 없습니다");
-        return (int) (i / j);
+        if(j==0) throw new IllegalArgumentException("[ERROR] 0으로 나눌 수 없습니다");
+        if(i / j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
+        return (int) Math.round(i / j);
     }
 
     public static double stringToDouble(String i) {
