19e7f7bedd1c0aece8b54fb76fd5e5484f034ea1
HyeonsuLee <leehyeonsu4888@naver.com>
2024-04-03 17:19:10 +0900

    feat: 테스트코드 DisplayName작성

--- src/test/java/CalculaterTest.java
+++ src/test/java/CalculaterTest.java
@@ -2,6 +2,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculaterTest {
@@ -9,24 +10,28 @@ class CalculaterTest {
     Calculater calculater = new Calculater();
 
     @Test
+    @DisplayName("두 숫자를 더하다.")
     void add() {
         int actual = calculater.add(1, 2);
         assertThat(actual).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("두 숫자를 빼다.")
     void subtract() {
         int actual = calculater.subtract(1, 2);
         assertThat(actual).isEqualTo(-1);
     }
 
     @Test
+    @DisplayName("두 숫자를 곱하다.")
     void multiply() {
         int actual = calculater.multiply(1, 2);
         assertThat(actual).isEqualTo(2);
     }
 
     @Test
+    @DisplayName("두 숫자를 나누다.")
     void divide() {
         int actual = calculater.divide(1, 2);
         assertThat(actual).isEqualTo(0);
--- src/test/java/StringSumCalculaterTest.java
+++ src/test/java/StringSumCalculaterTest.java
@@ -1,12 +1,18 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class StringSumCalculaterTest {
 
     StringSumCalculater stringSumCalculater = new StringSumCalculater();
 
     @Test
+    @DisplayName("배열의 모든 요소를 더한다.")
     void add() {
         int sum = stringSumCalculater.add("//;\n1:2,3;4");
         assertEquals(10, sum);
