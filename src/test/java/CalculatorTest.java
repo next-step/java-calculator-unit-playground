69f1984b099f72e47cc0c6231e841d15cc1b9e04
Seokju Hong <iamseokjuhong@gmail.com>
2024-04-04 09:06:02 +0900

    [계산기-1,2단계] 홍석주 미션 제출합니다 (#5)
    * 초간단 계산기 구현 (덧셈, 뺄셈, 곱셈, 나눗셈)
    
    * 2단계 - 초간단 계산기 테스트
    
    덧셈, 뺄셈, 곱셈, 나눗셈 테스트 코드 작성
    
    * 테스트의 설명들 구체화
    
    * 0으로 나눴을 때 예외 발생 테스트 작성
    
    * 과제 설명 작성
    
    ---------
    
    Co-authored-by: THINKBIG\10017612 <sj.hong@wjtb.net>

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,51 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("두 개의 정수를 더해본다")
+    void add() {
+        final int expected = 12;
+        final int actual = new Calculator().add(8, 4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("두 개의 정수를 빼본다")
+    void subtract() {
+        final int expected = 4;
+        final int actual = new Calculator().subtract(8, 4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("두 개의 정수를 곱해본다")
+    void multiply() {
+        final int expected = 32;
+        final int actual = new Calculator().multiply(8, 4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("두 개의 정수를 나눠본다")
+    void divide() {
+        final int expected = 2;
+        final int actual = new Calculator().divide(8, 4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("0으로 나눗셈을 하면 ArithmeticException 예외가 발생한다")
+    void divide_zero() {
+        assertThrows(ArithmeticException.class, () -> {
+            new Calculator().divide(8, 0);
+        });
+    }
+}
