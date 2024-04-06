eb826d4085e000cc5cacc31e279488dca945b091
JoungMinJu <alswn9938@gmail.com>
2024-04-06 21:46:58 +0900

    (step3) step1 계산기 명칭을 SimpleCalculator로 변경

---
+++ src/main/java/model/SimpleCalculator.java
@@ -0,0 +1,33 @@
+package model;
+
+public class SimpleCalculator {
+
+    public static int plus(double x, double y) {
+        if (x + y > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException();
+        }
+        return getValidResult(x + y);
+    }
+
+    public static int minus(double x, double y) {
+        return getValidResult(x - y);
+    }
+
+    public static int multiply(double x, double y) {
+        return getValidResult(x * y);
+    }
+
+    public static int divide(double x, double y) {
+        if (y == 0) {
+            throw new IllegalArgumentException("[ERROR] 0으로는 나눌 수 없습니다.");
+        }
+        return getValidResult(x / y);
+    }
+
+    private static int getValidResult(double input) {
+        if (input > Integer.MAX_VALUE || input < Integer.MIN_VALUE ) {
+            throw new IllegalArgumentException("[ERROR] 결과 값이 표현 가능한 정수 범위를 넘어갔습니다.");
+        }
+        return (int) input;
+    }
+}
---
+++ src/main/java/service/SimpleCalculateService.java
@@ -0,0 +1,41 @@
+package service;
+
+import model.SimpleCalculator;
+
+public class SimpleCalculateService {
+
+    public int plus(String strX, String strY) {
+        double x = parseDouble(strX);
+        double y = parseDouble(strY);
+        return SimpleCalculator.plus(x, y);
+    }
+
+    public int minus(String strX, String strY) {
+        double x = parseDouble(strX);
+        double y = parseDouble(strY);
+        return SimpleCalculator.minus(x, y);
+    }
+
+    public int multiply(String strX, String strY) {
+        double x = parseDouble(strX);
+        double y = parseDouble(strY);
+        return SimpleCalculator.multiply(x, y);
+    }
+
+    public int divide(String strX, String strY) {
+        double x = parseDouble(strX);
+        double y = parseDouble(strY);
+        return SimpleCalculator.divide(x, y);
+    }
+
+    private double parseDouble(String input) {
+        if (!isNumber(input)) {
+            throw new IllegalArgumentException("[ERROR] input 값 " + input + "은 숫자가 아닙니다.");
+        }
+        return Double.parseDouble(input);
+    }
+
+    private boolean isNumber(String input) {
+        return input != null && input.matches("[+-]?\\d+\\.?\\d*");
+    }
+}
