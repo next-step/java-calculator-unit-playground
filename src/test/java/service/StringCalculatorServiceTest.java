bb2c7c9b248fb0755a7cc1f1ff018669d24560f6
JoungMinJu <alswn9938@gmail.com>
2024-04-07 22:57:53 +0900

    (step4) SimpleCalculator 관련 테스트 리팩토링

--- src/test/java/service/StringCalculatorServiceTest.java
+++ src/test/java/service/StringCalculatorServiceTest.java
@@ -1,7 +1,7 @@
 package service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -26,7 +26,8 @@ class StringCalculatorServiceTest {
     @CsvSource({"//.\\n, .", "//~\\n, ~", "//123\\n, 123"})
     @DisplayName("//과 \\n 사이의 string은 커스텀 구분자이다")
     void testCustomSeparator(String expression, String expectedCustomSeparator) {
-        assertEquals(stringCalculatorService.getCustomSeparator(expression), expectedCustomSeparator);
+        assertThat(stringCalculatorService.getCustomSeparator(expression))
+            .isEqualTo(expectedCustomSeparator);
     }
 
     private static Stream<Arguments> validMethodSourceOfPlusAllTokens() {
@@ -40,22 +41,42 @@ class StringCalculatorServiceTest {
     @MethodSource("validMethodSourceOfPlusAllTokens")
     @DisplayName("유효한 String[] 숫자 배열이 주어지면 그 값을 모두 더한 값이 return 된다.")
     void validTestOfPlusAllTokens(List<String> tokens, int expectedResult) {
-        assertEquals(stringCalculatorService.plusAllTokens(tokens), expectedResult);
+        assertThat(stringCalculatorService.plusAllTokens(tokens))
+            .isEqualTo(expectedResult);
     }
 
     private static Stream<Arguments> invalidMethodSourceOfPlusAllTokens() {
         return Stream.of(
-            Arguments.arguments(List.of("-1", "0", "1")),
             Arguments.arguments(List.of("a", "1", "2")),
-            Arguments.arguments(List.of(".", "3"))
+            Arguments.arguments(List.of(".", "3")),
+            Arguments.arguments(List.of("가", "3"))
         );
     }
 
-    @ParameterizedTest(name = "숫자가 아니거나 음수가 포함된 값 {0}을 입력하면 에러가 반환된다.")
+    @ParameterizedTest(name = "숫자가 아닌 값 {0}을 입력하면 에러가 반환된다.")
     @MethodSource("invalidMethodSourceOfPlusAllTokens")
-    @DisplayName("숫자가 아니거나 음수인 값이 들어오면 에러가 반환된다.")
+    @DisplayName("숫자가 아닌 값이 들어오면 에러가 반환된다.")
     void invalidTestOfPlusAllTokens(List<String> tokens) {
-        assertThrows(IllegalArgumentException.class, () -> stringCalculatorService.plusAllTokens(tokens));
+        assertThatThrownBy(() -> stringCalculatorService.plusAllTokens(tokens))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(String.format("[ERROR] %s은(는) 유효한 숫자가 아닙니다.", tokens.get(0)));
+    }
+
+    private static Stream<Arguments> minusValueMethodSourceOfPlusAllTokens() {
+        return Stream.of(
+            Arguments.arguments(List.of("-1", "1", "2")),
+            Arguments.arguments(List.of("-2", "3")),
+            Arguments.arguments(List.of("-3", "3"))
+        );
+    }
+
+    @ParameterizedTest(name = "음수 {0}을 입력하면 에러가 반환된다.")
+    @MethodSource("minusValueMethodSourceOfPlusAllTokens")
+    @DisplayName("음수인 값이 들어오면 에러가 반환된다.")
+    void minusValueTestOfPlusAllTokens(List<String> tokens) {
+        assertThatThrownBy(() -> stringCalculatorService.plusAllTokens(tokens))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(String.format("[ERROR] 음수 값 %s은(는) 입력하실 수 없습니다.", tokens.get(0)));
     }
 
     private static Stream<Arguments> methodSourceOfGetTokens() {
@@ -71,7 +92,8 @@ class StringCalculatorServiceTest {
     @MethodSource("methodSourceOfGetTokens")
     @DisplayName("기본 구분자 ','와 ':' 그리고 커스텀 구분자를 활용하여 더할 숫자들의 집합을 구할 수 있다.")
     void getTokensTest(String expression, List<String> expectedTokens) {
-        assertEquals(stringCalculatorService.getTokens(expression), expectedTokens);
+        assertThat(stringCalculatorService.getTokens(expression))
+            .containsExactlyElementsOf(expectedTokens);
     }
 
     private static Stream<Arguments> methodSourceOfCalculateTest() {
@@ -86,7 +108,8 @@ class StringCalculatorServiceTest {
     @MethodSource("methodSourceOfCalculateTest")
     @DisplayName("기본 구분자와 커스텀 구분자를 활용하여 숫자를 구분해내고 그 합을 정상적으로 반환할 수 있다.")
     void calculateTest(String expression, int expectedResult) {
-        assertEquals(stringCalculatorService.calculate(expression), expectedResult);
+        assertThat(stringCalculatorService.calculate(expression))
+            .isEqualTo(expectedResult);
     }
 
 }
