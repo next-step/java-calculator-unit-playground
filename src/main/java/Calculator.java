052b25b5988e8521874f4eb40f2b70fa990e1958
Seonghwan Kim <109322241+fanngineer@users.noreply.github.com>
2024-04-10 05:17:17 +0900

    Revert "계산기 Step3, Step4"

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,39 +3,29 @@ public class Calculator {
     public static int add(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-        if(i + j > Integer.MAX_VALUE || i+j <Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
-        }
+        if(i + j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
         return (int) Math.round(i + j);
     }
 
     public static int subtract(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-        if(i - j > Integer.MAX_VALUE || i-j <Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
-        }
+        if(i - j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
         return (int) Math.round(i - j);
     }
 
     public static int multiply(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-        if(i * j > Integer.MAX_VALUE || i*j <Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
-        }
+        if(i * j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
         return (int) Math.round(i * j);
     }
 
     public static int divide(String a, String b) {
         double i = stringToDouble(a);
         double j = stringToDouble(b);
-        if(j==0) {
-            throw new IllegalArgumentException("[ERROR] 0으로 나눌 수 없습니다");
-        }
-        if(i / j > Integer.MAX_VALUE || i/j <Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
-        }
+        if(j==0) throw new IllegalArgumentException("[ERROR] 0으로 나눌 수 없습니다");
+        if(i / j > Integer.MAX_VALUE) throw new IllegalArgumentException("[ERROR]결과값이 Integer 범위 이상입니다.");
         return (int) Math.round(i / j);
     }
 
