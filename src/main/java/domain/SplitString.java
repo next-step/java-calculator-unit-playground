11ae7a4873dc6b56773c2c8d03b27342960922e1
승엽 <tmdduqflfl@naver.com>
2024-05-06 16:51:19 +0900

    refactor :  갈아 엎기 전 어떤 상태인지 알기 위한 커밋

--- src/main/java/domain/SplitString.java
+++ src/main/java/domain/SplitString.java
@@ -2,29 +2,44 @@ package domain;
 
 public class SplitString {
 
-    private final String DISTINCTION = ":|@|,";
-    private int i;
-    private final int INITIAL_NUMBER = 0;
-    public int[] intArr;
+    private static final int INITIAL_NUMBER = 0;
+    private static final String START_STRING = "//";
+    private static final String END_STRING = "\n";
+    private static int START_INT_NUM = 2;
+    private static int END_INT_NUM = 3;
+    private static int START_NUM = 5;
 
-    public void splitStr(String str) {
-        String[] strArr = str.split(DISTINCTION);
-        int[] intArr = new int[strArr.length];
-        Exception(strArr, intArr);
+    public static int[] intArr;
+    public String[] strArr;
+
+    public int[] splitStr(String str) {
+        splitException(str);
+        intArr = new int[strArr.length];
+        return intArr;
     }
 
-    private void Exception(String[] strArr, int[] intArr) {
-        for (i = INITIAL_NUMBER; i < strArr.length; i++) {
+    private void splitException(String str) {
+        if (str.startsWith(START_STRING) && str.endsWith(END_STRING)) {
+            String basicSplit = str.substring(START_INT_NUM, END_INT_NUM);
+            strArr = str.substring(START_NUM).split(basicSplit);
+            Exception(strArr);
+        }
+    }
+
+    private static void Exception(String[] strArr) {
+        for (int i = INITIAL_NUMBER; i < strArr.length; i++) {
             try {
                 intArr[i] = Integer.parseInt(strArr[i]);
-                if (intArr[i] < 0) {
-                    throw new IllegalArgumentException();
-                }
+                validateMinus(intArr, i);
             } catch (NumberFormatException e) {
                 throw new RuntimeException();
             }
         }
     }
 
-
+    private static void validateMinus(int[] intArr, int i) {
+        if (intArr[i] < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
