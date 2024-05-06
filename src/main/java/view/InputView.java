c178b3e0298a786f64eef4f8b428b83f6ab00040
승엽 <tmdduqflfl@naver.com>
2024-05-07 01:13:28 +0900

    refactor : InputView 에서 스캐너를 사용함으로써 마지막 Controller에서 최종적으로 사용하기위해 구현해봤습니다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,7 +1,13 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
-    public void printing() {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public String calculatorInput() {
         System.out.println("구분자를 넣어 계산할 문자열을 넣으시오:");
+        return scanner.nextLine();
     }
 }
