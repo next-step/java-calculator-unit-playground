d097f1ce8d5589d5b2c6bf0130b5678830efab2f
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-05-08 03:52:36 +0900

    Feat: 문자열 계산기 구현

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -1,5 +1,26 @@
 package controller;
 
+import domain.StringCalculator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
 public class CalculatorController {
 
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void startCalculator(){
+        String inputStrings = inputView.getString();
+        StringCalculator stringCalculator = new StringCalculator(inputStrings);
+        List<String> paresStrings = stringCalculator.parseStrings();
+        List<Integer> numberList = stringCalculator.changeStringsToNumbers(paresStrings);
+        outputView.printNumber(stringCalculator.addNumbers(numberList));
+    }
 }
--- src/main/java/domain/StringCalculator.java
+++ src/main/java/domain/StringCalculator.java
@@ -1,7 +1,65 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
+    private final String STANDARD_SPLIT_STRINGS = "[,|:]";
+    private final int INITIAL_NUMBER = 0;
+    private final int FIRST_NUMBER = 1;
+    private final int SECOND_NUMBER = 2;
+    private final int THIRD_NUMBER = 3;
+    private final int CUSTOM_STRING_START_NUMBER = 5;
+    private final String EXCEPTION_MINUS = "음수 입력 불가.";
+    private final String EXCEPTION_INCORRECT = "제대로 입력하세요.";
+    private final String CHECK_CUSTOM_OR_STANDARD = "/";
+    private final String inputStrings;
 
+    public StringCalculator(final String inputStrings) {
+        this.inputStrings = inputStrings;
+    }
+
+    public List<String> parseStrings() {
+        String checkCustomOrStandard = inputStrings.substring(INITIAL_NUMBER, FIRST_NUMBER);
+        if (!checkCustomOrStandard.equals(CHECK_CUSTOM_OR_STANDARD)) {
+            String[] splitStrings = inputStrings.split(STANDARD_SPLIT_STRINGS);
+            return List.of(splitStrings);
+        }
+        String delimiter = inputStrings.substring(SECOND_NUMBER, THIRD_NUMBER);
+        String divideStrings = inputStrings.substring(CUSTOM_STRING_START_NUMBER);
+        String[] splitStrings = divideStrings.split(delimiter);
+        return List.of(splitStrings);
+    }
+
+    public List<Integer> changeStringsToNumbers(List<String> splitStrings) {
+        List<Integer> numberList = new ArrayList<>();
+        try {
+            for (String stringNumber : splitStrings) {
+                Integer number = Integer.parseInt(stringNumber);
+                numberList.add(number);
+            }
+        } catch (RuntimeException runtimeException) {
+            throw new RuntimeException(EXCEPTION_INCORRECT);
+        }
+        validateMinusNumber(numberList);
+        return numberList;
+    }
+
+    private void validateMinusNumber(List<Integer> numberList) {
+        for (Integer realNumber : numberList) {
+            if (realNumber < 0) {
+                throw new RuntimeException(EXCEPTION_MINUS);
+            }
+        }
+    }
+
+    public int addNumbers(List<Integer> numberList) {
+        int sum = INITIAL_NUMBER;
+        for (int number : numberList) {
+            sum += number;
+        }
+        return sum;
+    }
 
 }
