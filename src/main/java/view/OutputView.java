9a11ab8aabbcc082ecff03fdf83eef35afe87981
승엽 <tmdduqflfl@naver.com>
2024-05-07 14:35:35 +0900

    refactor : 배열을 사용하다가, 리스트로 변환해서 진행했습니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,35 +1,31 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OutputView {
-    private final String DISTINCTION_FRONT = "//";
-    private final String DISTINCTION_BACK = "\n";
-    private final int INITIAL_NUMBER = 0;
-    private final int START_NUM = 2;
-    private final int NEXT_START_NUM = 1;
 
-    private int i;
-    private int[] intArr;
+    private static final String DISTINCTION_FRONT = "//";
+    private static final String DISTINCTION_BACK = "\n";
+    private static final int FOR_DISTINCTION_NUM = 1;
 
-    public String splitStr(String str) {
+    private ArrayList<Integer> parsedValues = new ArrayList<>();
+
+    public List<Integer> parseString(String str) {
+
         if (str.startsWith(DISTINCTION_FRONT) && str.contains(DISTINCTION_BACK)) {
-            String extract = str.substring(START_NUM, str.indexOf(DISTINCTION_BACK));
-            String[] strArr = str.substring(str.indexOf(DISTINCTION_BACK) + NEXT_START_NUM).split(extract);
-            intArr = new int[strArr.length];
-            Exception(strArr);
-        }
-        return str;
-    }
+            String separator = str.substring(DISTINCTION_FRONT.length(), DISTINCTION_FRONT.length() + FOR_DISTINCTION_NUM);
+            String numbersList = str.substring(str.indexOf(DISTINCTION_BACK) + FOR_DISTINCTION_NUM);
+            String[] numbersArray = numbersList.split(separator);
 
-    private void Exception(String[] strArr) {
-        for (i = INITIAL_NUMBER; i < strArr.length; i++) {
-            try {
-                intArr[i] = Integer.parseInt(strArr[i]);
-                if (intArr[i] < 0) {
-                    throw new IllegalArgumentException();
+            for (String num : numbersArray) {
+                try {
+                    int parsedNum = Integer.parseInt(num.trim());
+                    parsedValues.add(parsedNum);
+                } catch (NumberFormatException e) {
                 }
-            } catch (NumberFormatException e) {
-                throw new RuntimeException();
             }
         }
+        return parsedValues;
     }
 }
