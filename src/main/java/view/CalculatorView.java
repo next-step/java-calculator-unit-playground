3bfb2cf1f0b2b6d079e0f2e6e4efa85b9b1e9159
mlngwan <david027301@naver.com>
2024-05-08 21:40:03 +0900

    Rename: 파일명 수정

--- src/main/java/view/CalculatorView.java
+++ src/main/java/view/CalculatorView.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class InputView {
+public class CalculatorView {
     public static List<Integer> getNumber(){
         Scanner input = new Scanner(System.in);
         System.out.println("두 정수를 입력하세요. (입력 순서대로 계산, 쉼표(,)로 구분)");
-        String inputNumber = input.next();
-        String[] inputNumberList = inputNumber.split(",");
+        String userInput = input.next();
+        String[] inputNumberList = userInput.split(",");
         List<Integer> numbers = new ArrayList<>();
         for (String intList : inputNumberList){
             numbers.add(Integer.parseInt(intList));
