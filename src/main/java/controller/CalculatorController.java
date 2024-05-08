ed9a15193f6158b9daaa31b892484a76bbbf7d29
mlngwan <david027301@naver.com>
2024-05-08 21:40:31 +0900

    feat: MVC 구조를 위한 Controller 추가

---
+++ src/main/java/controller/CalculatorController.java
@@ -0,0 +1,29 @@
+package controller;
+import model.Calculator;
+import view.CalculatorView;
+import java.util.List;
+
+public class CalculatorController {
+    List<Integer> numbers = CalculatorView.getNumber();
+    Calculator calculator = new Calculator(numbers.get(0), numbers.get(1));
+
+    public void doAdd() {
+        int addResult = calculator.addNumbers();
+        System.out.println("덧셈 결과 : " + addResult);
+    }
+
+    public void doSub(){
+        int subResult = calculator.subNumbers();
+        System.out.println("뺄셈 결과 : " + subResult);
+    }
+
+    public void doDivide(){
+        int divideResult = calculator.divideNumbers();
+        System.out.println("나눗셈 결과 : " + divideResult);
+    }
+
+    public void doMultiple(){
+        int multipleResult = calculator.multipleNumbers();
+        System.out.println("곱셈 결과 : " + multipleResult);
+    }
+}
