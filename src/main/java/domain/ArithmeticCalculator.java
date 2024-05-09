f885a24f9a354fd3368aff65ba78206d9441e21f
승엽 <tmdduqflfl@naver.com>
2024-05-09 15:46:03 +0900

    refactor : - 응집도를 높이기 위한 메서드 분리 - static 지양을 위한 수정 - 캡슐화를 위한 public->private 수정 private 수정 후 get으로 입력값 받기 완료

--- src/main/java/domain/ArithmeticCalculator.java
+++ src/main/java/domain/ArithmeticCalculator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class ArithmeticCalculator {
 
-    private static final int INITIAL_RESULT_NUM=0;
+    private static final int INITIAL_RESULT_NUM = 0;
     private static final int INITIAL_NUMBER = 1;
 
     private final ArrayList<Integer> numbers;
@@ -15,21 +15,21 @@ public class ArithmeticCalculator {
         this.result = INITIAL_RESULT_NUM;
     }
 
-    public int plusCalculate() {
+    private int plusCalculate() {
         for (Integer number : numbers) {
             result += number;
         }
         return result;
     }
 
-    public int minusCalculate() {
+    private int minusCalculate() {
         for (Integer number : numbers) {
             result -= number;
         }
         return result;
     }
 
-    public int divideCalculate() {
+    private int divideCalculate() {
         result = numbers.get(0);
         for (int i = 1; i < numbers.size(); i++) {
             result /= numbers.get(i);
@@ -37,11 +37,27 @@ public class ArithmeticCalculator {
         return result;
     }
 
-    public int multipleCalculate() {
+    private int multipleCalculate() {
         result = INITIAL_NUMBER;
         for (Integer number : numbers) {
             result *= number;
         }
         return result;
+    }
+
+    public int getPlusResult() {
+        return plusCalculate();
+    }
+
+    public int getMinusResult() {
+        return minusCalculate();
+    }
+
+    public int getDivideResult() {
+        return divideCalculate();
+    }
+
+    public int getMultipleResult() {
+        return multipleCalculate();
     }
 }
