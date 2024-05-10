90359c2043e309693ac79d4bdef2b44b122d2509
Hheojiwon <0618amy@naver.com>
2024-05-10 10:41:34 +0900

    feat: 문자열 계산기 기능

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -20,6 +20,40 @@ public class Calculator {
         return numArr;
     }
 
+    private String findCustomSplit(String s){
+        if (s.contains(SPLIT_CHAR1)&& s.contains(SPLIT_CHAR2)){
+            customSplitString = s.substring(s.indexOf(SPLIT_CHAR1), s.indexOf(SPLIT_CHAR2));
+        }
+        return customSplitString;
+    }
+
+    private String[] splitCustomSplit(String s){
+        numArr=s.split(s);
+        return numArr;
+    }
+
+    private static int getSum(String[] str){
+        int sum=0;
+        for (String num:str){
+            sum+=Integer.parseInt(num);
+        }
+        return sum;
+    }
+
+
+    public void StringCalculator(String s){
+        if (s.contains(",")||s.contains(":")){
+            numArr=splitString(s);
+        }
+        else{
+            numArr=splitCustomSplit(findCustomSplit(s));
+        }
+    }
+
+    public static int getAnswer(){
+        return getSum(numArr);
+    }
+
 
     public int add(int a, int b){
         result=a+b;
