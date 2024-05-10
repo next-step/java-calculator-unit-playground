0bd43b789662d60ba7485453dd766c99cd1d299d
Hheojiwon <0618amy@naver.com>
2024-05-10 10:38:38 +0900

    feat: 문자열 입력받는 메서드

---
+++ src/main/java/view/Input.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class Input {
+    public static Scanner sc=new Scanner(System.in);
+    public static String getString(){
+        return sc.nextLine();
+    }
+
+}
