25dce1cab20167916418152ec626749d4b4c19a9
Hheojiwon <0618amy@naver.com>
2024-05-10 10:36:45 +0900

    feat: 문자열 구분 메서드

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -1,7 +1,25 @@
 package domain;
 
+import java.rmi.NoSuchObjectException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import java.lang.String;
+
+
 public class Calculator {
     private static int result;
+    private static final String SPLIT_CHAR1="//";
+    private static final String SPLIT_CHAR2="|n";
+    private static String customSplitString="";
+
+    static String[] numArr=null;
+
+    private String[] splitString(String s){
+        numArr= s.split("[,|:]");
+        return numArr;
+    }
+
 
     public int add(int a, int b){
         result=a+b;
