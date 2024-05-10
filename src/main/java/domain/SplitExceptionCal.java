f992555ccb8e9c149ad8f6551ac198e7e85f108d
승엽 <tmdduqflfl@naver.com>
2024-05-10 20:56:52 +0900

    Fix :

--- src/main/java/domain/SplitExceptionCal.java
+++ src/main/java/domain/SplitExceptionCal.java
@@ -5,32 +5,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SplitExceptionCal {
-
+    private static final String FOR_SPLIT="//|\\n";
     private static final int DISTINCTION_FRONT = 1;
     private static final int DISTINCTION_BACK = 2;
 
-    private List<String> parsedValue;
+    private List<String> parsedValue = new ArrayList<>();
 
-    private SplitExceptionCal(String input) {
-        parsedValue = parseInput(input);
+    public List<String> getParseInput(String input) {
+        return parseInput(input);
+    }    //"//;\n1;2;3"
+
+    public List<String> getParseException(List<String> values) {
+        return parseException(values);
     }
 
-    //"//;\n1;2;3"
     private List<String> parseInput(String input) {
-        List<String> parsedValue = new ArrayList<>();
-        String[] parts = input.split("//|\\n");
+        String[] parts = input.split(FOR_SPLIT);
         String delimiter = parts[DISTINCTION_FRONT];
         String[] values = parts[DISTINCTION_BACK].split(delimiter);
         return Arrays.asList(values);
     }
 
-    private void parseException(String value) {
+    private List<String> parseException(List<String> values) {
         try {
-            int parsedNumber = Integer.parseInt(value);
+            int parsedNumber = Integer.parseInt(values.toString());
             parsedValue.add(String.valueOf(parsedNumber));
         } catch (NumberFormatException e) {
             throw new RuntimeException(e);
         }
+        return parsedValue;
     }
-
 }
