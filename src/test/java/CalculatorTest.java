764c287ca0c419e2e1b31c630e728a32ac832a28
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-05-10 22:28:40 +0900

    Refactor: 테스트 수정
    올바른 경우의 테스트가 검증된 메서드의 잘못된 경우의 테스트 삭제, @Display 어노테이션으로 인해 테스트 메서드 이름과 겹치는 경우 @Display 어노테이션 삭제

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    @DisplayName("더하기 테스트")
     void 더하기_테스트() {
         //given
         final Calculator calculator = new Calculator();
@@ -23,23 +22,6 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("잘못된 더하기 테스트")
-    void 잘못된_더하기_테스트() {
-        //given
-        final Calculator calculator = new Calculator();
-        final int num1 = 1;
-        final int num2 = 2;
-        final int unexpected = 4;
-
-        //when
-        final int actual = calculator.add(num1, num2);
-
-        //then
-        assertNotEquals(unexpected, actual);
-    }
-
-    @Test
-    @DisplayName("빼기 테스트")
     void 빼기_테스트() {
         //given
         final Calculator calculator = new Calculator();
@@ -55,23 +37,6 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("잘못된 빼기 테스트")
-    void 잘못된_빼기_테스트() {
-        //given
-        final Calculator calculator = new Calculator();
-        final int num1 = 1;
-        final int num2 = 2;
-        final int unexpected = 0;
-
-        //when
-        final int actual = calculator.subtract(num1, num2);
-
-        //then
-        assertNotEquals(unexpected, actual);
-    }
-
-    @Test
-    @DisplayName("곱하기 테스트")
     void 곱하기_테스트() {
         //given
         final Calculator calculator = new Calculator();
@@ -87,23 +52,6 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("잘못된 곱하기 테스트")
-    void 잘못된_곱하기_테스트() {
-        //given
-        final Calculator calculator = new Calculator();
-        final int num1 = 1;
-        final int num2 = 2;
-        final int expected = 0;
-
-        //when
-        final int actual = calculator.multiple(num1, num2);
-
-        //then
-        assertNotEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("나누기 테스트")
     void 나누기_테스트() {
         //given
         final Calculator calculator = new Calculator();
@@ -119,7 +67,6 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("분모가 0일때 나누기 테스트")
     void 분모가_0일때_나누기_테스트() {
         //given
         final Calculator calculator = new Calculator();
@@ -133,21 +80,4 @@ class CalculatorTest {
         //then
         assertEquals(expected, arithmeticException.getMessage());
     }
-
-    @Test
-    @DisplayName("잘못된_나누기 테스트")
-    void 잘못된_나누기_테스트() {
-        //given
-        final Calculator calculator = new Calculator();
-        final int num1 = 1;
-        final int num2 = 2;
-        final int expected = 1;
-
-        //when
-        final int actual = calculator.divide(num1, num2);
-
-        //then
-        assertNotEquals(expected, actual);
-    }
-
 }
