ca370299133e70203c77d7c635a8b2ac5b61373e
Hheojiwon <0618amy@naver.com>
2024-05-12 05:04:33 +0900

    feat: 문자열 계산 메서드 추가, 코드 수정 사칙연산 계산기 코드 수정

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -13,45 +13,75 @@ public class Calculator {
     private static final String SPLIT_CHAR2="|n";
     private static String customSplitString="";
 
-    static String[] numArr=null;
+    static String[] strArr=null;
+    static int[] numArr=null;
 
     private String[] splitString(String s){
-        numArr= s.split("[,|:]");
-        return numArr;
+        strArr= s.split("[,|:]");
+        return strArr;
     }
 
     private String findCustomSplit(String s){
         if (s.contains(SPLIT_CHAR1)&& s.contains(SPLIT_CHAR2)){
-            customSplitString = s.substring(s.indexOf(SPLIT_CHAR1), s.indexOf(SPLIT_CHAR2));
+            customSplitString = s.substring(s.indexOf(SPLIT_CHAR1)+2, s.indexOf(SPLIT_CHAR2));
         }
         return customSplitString;
     }
 
     private String[] splitCustomSplit(String s){
-        numArr=s.split(s);
+        customSplitString=findCustomSplit(s);
+        s=s.substring(s.indexOf(customSplitString)+3);
+        strArr=s.split(customSplitString);
+        return strArr;
+    }
+
+    public int[] stringToInteger(String[] s){
+        try{
+            for (int i=0;i< strArr.length;i++){
+                numArr[i]=Integer.parseInt(strArr[i]);
+            }
+        } catch (RuntimeException runtimeException){
+            throw new RuntimeException("숫자를 입력해주세요.");
+        }
         return numArr;
     }
 
-    private static int getSum(String[] str){
+    private int getSum(String[] strArr){
+        numArr=stringToInteger(strArr);
         int sum=0;
-        for (String num:str){
-            sum+=Integer.parseInt(num);
+        for (int num:numArr){
+            if (num<0){
+                throw new RuntimeException("음수를 입력할 수 없습니다.");
+            }
+            sum+=num;
         }
         return sum;
     }
 
-
-    public void StringCalculator(String s){
+    public int StringCalculator(String s) {
+        try {
+            blankString(s);
+        } catch (NoSuchObjectException e) {
+            throw new RuntimeException();
+        }
         if (s.contains(",")||s.contains(":")){
-            numArr=splitString(s);
+            strArr=splitString(s);
         }
         else{
-            numArr=splitCustomSplit(findCustomSplit(s));
+            customSplitString=findCustomSplit(s);
+            strArr=splitCustomSplit(s);
+        }
+        return getSum(strArr);
+    }
+
+    public void blankString(String s) throws NoSuchObjectException{
+        if (s.isEmpty()){
+            throw new NoSuchObjectException("공백은 입력할 수 없습니다.");
         }
     }
 
-    public static int getAnswer(){
-        return getSum(numArr);
+    public int[] getNumArr(){
+        return numArr;
     }
 
 
@@ -71,6 +101,9 @@ public class Calculator {
     }
 
     public int divide(int a, int b){
+        if (b==0){
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
         result=a/b;
         return result;
     }
