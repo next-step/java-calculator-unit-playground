c8f3b456878b87b1e3b2b1b158e2bd8c29712faf
승엽 <tmdduqflfl@naver.com>
2024-05-12 19:59:09 +0900

    Refactor : - 최대한 클린 코드를 지키며 수정했습니다.

--- src/main/java/domain/ArithmeticCalculator.java
+++ src/main/java/domain/ArithmeticCalculator.java
@@ -1,63 +1,47 @@
 package domain;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ArithmeticCalculator {
 
     private static final int INITIAL_RESULT_NUM = 0;
     private static final int INITIAL_NUMBER = 1;
 
-    private final ArrayList<Integer> numbers;
-    private int result;
+    private final List<Integer> numbers;
 
-    public ArithmeticCalculator(ArrayList<Integer> numbers) {
+    public ArithmeticCalculator(List<Integer> numbers) {
         this.numbers = numbers;
-        this.result = INITIAL_RESULT_NUM;
     }
 
-    private int plusCalculate() {
+    public int plusCalculate() {
+        int result = INITIAL_RESULT_NUM;
         for (Integer number : numbers) {
             result += number;
         }
         return result;
     }
 
-    private int minusCalculate() {
+    public int minusCalculate() {
+        int result = INITIAL_RESULT_NUM;
         for (Integer number : numbers) {
             result -= number;
         }
         return result;
     }
 
-    private int divideCalculate() {
-        result = numbers.get(0);
-        for (int i = 1; i < numbers.size(); i++) {
+    public int divideCalculate() {
+        int result = numbers.get(INITIAL_RESULT_NUM);
+        for (int i = INITIAL_NUMBER; i < numbers.size(); i++) {
             result /= numbers.get(i);
         }
         return result;
     }
 
-    private int multipleCalculate() {
-        result = INITIAL_NUMBER;
+    public int multipleCalculate() {
+        int result = INITIAL_NUMBER;
         for (Integer number : numbers) {
             result *= number;
         }
         return result;
-    }
-
-    public int getPlusResult() {
-        return plusCalculate();
-    }
-
-    public int getMinusResult() {
-        return minusCalculate();
-    }
-
-    public int getDivideResult() {
-        return divideCalculate();
-    }
-
-    public int getMultipleResult() {
-        return multipleCalculate();
     }
 }
