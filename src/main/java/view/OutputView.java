3e2c6d72c758f43efc4f8657ae920fd23247de57
승엽 <tmdduqflfl@naver.com>
2024-05-12 20:02:53 +0900

    fix: - 버그 수정 중 바뀐 것들

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,31 +1,15 @@
 package view;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-public class OutputView {
+public class Outputview {
 
-    private static final String DISTINCTION_FRONT = "//";
-    private static final String DISTINCTION_BACK = "\n";
-    private static final int FOR_DISTINCTION_NUM = 1;
+    private static final Scanner scanner = new Scanner(System.in);
 
-    private ArrayList<Integer> parsedValues = new ArrayList<>();
-
-    public List<Integer> parseString(String str) {
-
-        if (str.startsWith(DISTINCTION_FRONT) && str.contains(DISTINCTION_BACK)) {
-            String separator = str.substring(DISTINCTION_FRONT.length(), DISTINCTION_FRONT.length() + FOR_DISTINCTION_NUM);
-            String numbersList = str.substring(str.indexOf(DISTINCTION_BACK) + FOR_DISTINCTION_NUM);
-            String[] numbersArray = numbersList.split(separator);
-
-            for (String num : numbersArray) {
-                try {
-                    int parsedNum = Integer.parseInt(num.trim());
-                    parsedValues.add(parsedNum);
-                } catch (NumberFormatException e) {
-                }
-            }
-        }
-        return parsedValues;
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        String input = inputView.getCalculator();
+        System.out.println("계산 결과:");
+        System.out.println(input);
     }
 }
