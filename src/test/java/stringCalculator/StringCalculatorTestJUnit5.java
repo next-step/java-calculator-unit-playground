eb5a102794e1c8e6255245dfe13f55632e7800ad
mlngwan <david027301@naver.com>
2024-05-13 11:26:43 +0900

    Rename: Test Code 위치 변경 및 파일 이름 수정

--- src/test/java/stringCalculator/StringCalculatorTestJUnit5.java
+++ src/test/java/stringCalculator/StringCalculatorTestJUnit5.java
@@ -1,15 +1,19 @@
 package stringCalculator;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import stringCalcculator.model.StringCalculator;
+import simpleCalculator.model.StringCalculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class StringCalculatorTest {
+public class StringCalculatorTestJUnit5 {
+    
     public static class stringTest {
+        
         @Test
         @DisplayName("값이 없거나 비어있는 경우")
-        public void nullAndEmpty(){
+        public void nullAndEmpty() {
+            
             StringCalculator calculator = new StringCalculator();
             assertEquals(0, calculator.add(null));
             assertEquals(0, calculator.add(""));
@@ -17,7 +21,8 @@ public class StringCalculatorTest {
 
         @Test
         @DisplayName("기본 구분자를 사용하는 경우")
-        public void addWithDefault(){
+        public void addWithDefault() {
+            
             StringCalculator calculator = new StringCalculator();
             assertEquals(6, calculator.add("1,2,3"));
             assertEquals(6, calculator.add("1,2:3"));
@@ -27,14 +32,16 @@ public class StringCalculatorTest {
 
         @Test
         @DisplayName("커스텀 구분자를 사용하는 경우")
-        public void addWithCustom(){
+        public void addWithCustom() {
+            
             StringCalculator calculator = new StringCalculator();
             assertEquals(6, calculator.add("//;\n1;2;3"));
         }
 
         @Test
         @DisplayName("숫자가 아닌 다른 값을 입력한 경우")
-        public void isNotNumber(){
+        public void isNotNumber() {
+            
             StringCalculator calculator = new StringCalculator();
             assertThrows(RuntimeException.class, () -> calculator.add("hi,1,2"));
             assertThrows(RuntimeException.class, () -> calculator.add("hi"));
@@ -42,11 +49,11 @@ public class StringCalculatorTest {
 
         @Test
         @DisplayName("음수를 입력한 경우")
-        public void isNegative(){
+        public void isNegative() {
+            
             StringCalculator calculator = new StringCalculator();
             assertThrows(RuntimeException.class, () -> calculator.add("-2,1,2"));
             assertThrows(RuntimeException.class, () -> calculator.add("-2"));
-
         }
     }
 }
