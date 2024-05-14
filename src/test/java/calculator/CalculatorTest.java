78c99494ef95499b74510265b74cabfcb03d0b98
mlngwan <david027301@naver.com>
2024-05-15 03:49:26 +0900

    Test: 리팩토링 후 새로운 Test 코드 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,145 +1,79 @@
 package calculator;
 
-import simpleCalculator.model.Calculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import simpleCalculator.view.CalculatorView;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import simpleCalculator.model.Calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+
+    private static final int EXPECT_VALUE = 3;
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setCalculator() {
+        calculator = new Calculator();
+    }
+
+    @DisplayName("연산기능 테스트")
     @Nested
-    @DisplayName("입력관련 테스트")
-    public class InputTest {
+    class functionTest{
 
-        @DisplayName("입력내용 저장 테스트")
+        @DisplayName("더하기")
         @Test
-        void inputAndSaveTest() {
+        void testAdd() {
+            calculator.setNumber("1,2");
+            assertEquals(EXPECT_VALUE, calculator.addNumbers());
+        }
 
-            //given
-            InputStream input = System.in;
-            ByteArrayInputStream in = new ByteArrayInputStream("2,1".getBytes());
-            System.setIn(in);
-            List<Integer> testList = CalculatorView.getNumber();
-            int num1 = testList.get(0);
-            int num2 = testList.get(1);
-            //when
-            //Then
-            assertEquals(2, num1, "값이 일치하지 않습니다.");
-            assertEquals(1, num2, "값이 일치하지 않습니다.");
-            assertNotEquals(1, num1, "값이 일치합니다!");
-            assertNotEquals(2, num2, "값이 일치합니다!");
+        @DisplayName("빼기")
+        @Test
+        void testSub() {
+            calculator.setNumber("4,1");
+            assertEquals(EXPECT_VALUE, calculator.subNumbers());
+        }
+
+        @DisplayName("나누기")
+        @Test
+        void testDiv() {
+            calculator.setNumber("9,3");
+            assertEquals(EXPECT_VALUE, calculator.divideNumbers());
+        }
+
+        @DisplayName("곱하기")
+        @Test
+        void testAMul() {
+            calculator.setNumber("3,1");
+            assertEquals(EXPECT_VALUE, calculator.multipleNumbers());
         }
     }
 
+    @DisplayName("예외처리 테스트")
     @Nested
-    @DisplayName("계산기능 테스트")
-    public class functionTest {
+    class exceptionTest {
 
-        int testNum1 = 2;
-        int testNum2 = 1;
-
-        Calculator calculator = new Calculator(testNum1, testNum2);
-
-        @DisplayName("더하기 테스트 : 예측값과 입력값이 같은 경우")
+        @DisplayName("0으로 나누기를 시도한 경우")
         @Test
-        void addTest() {
-
-            //given
-            int expectValue = 3;
-            int testAdd = calculator.addNumbers();
-            //when
-            //then
-            assertEquals(testAdd, expectValue, "값이 일치하지 않습니다.");
+        void testDivideByZero() {
+            calculator.setNumber("10,0");
+            assertThrows(RuntimeException.class, () -> calculator.divideNumbers());
         }
 
-        @DisplayName("더하기 테스트 : 예측값과 입력값이 다른 경우")
+        @DisplayName("입력을 2개 미만으로 시도한 경우")
         @Test
-        void addFailTest() {
-
-            //given
-            int failValue = 0;
-            int testAdd = calculator.addNumbers();
-            //when
-            //then
-            assertNotEquals(testAdd, failValue, "값이 일치합니다!");
+        void testInputUnderTwo() {
+            assertThrows(RuntimeException.class, () ->  calculator.setNumber("1"));
         }
 
-        @DisplayName("빼기 테스트 : 예측값과 입력값이 같은 경우")
+        @DisplayName("입력을 2개 초과로 시도한 경우")
         @Test
-        void subTest() {
-
-            //given
-            int expectValue = 1;
-            int testSub = calculator.subNumbers();
-            //when
-            //then
-            assertEquals(testSub, expectValue, "값이 일치하지 않습니다.");
-        }
-
-        @DisplayName("빼기 테스트 : 예측값과 입력값이 다른 경우")
-        @Test
-        void subFailTest() {
-
-            //given
-            int failValue = 0;
-            int testSub = calculator.addNumbers();
-            //when
-            //then
-            assertNotEquals(testSub, failValue, "값이 일치합니다!");
-        }
-
-        @DisplayName("나누기 테스트 : 예측값과 입력값이 같은 경우")
-        @Test
-        void divideTest() {
-
-            //given
-            int expectValue = 2;
-            int testDivide = calculator.divideNumbers();
-            //when
-
-            //then
-            assertEquals(testDivide, expectValue, "값이 일치하지 않습니다.");
-        }
-
-        @DisplayName("나누기 테스트 : 예측값과 입력값이 다른 경우")
-        @Test
-        void divideFailTest() {
-
-            //given
-            int failValue = 0;
-            int testDivide = calculator.addNumbers();
-            //when
-            //then
-            assertNotEquals(testDivide, failValue, "값이 일치합니다!");
-        }
-
-        @DisplayName("곱하기 테스트 : 예측값과 입력값이 같은 경우")
-        @Test
-        void multipleTest() {
-
-            //given
-            int expectValue = 2;
-            int testMultiple = calculator.multipleNumbers();
-            //when
-            //then
-            assertEquals(testMultiple, expectValue, "값이 일치하지 않습니다.");
-        }
-
-        @DisplayName("곱하기 테스트 : 예측값과 입력값이 다른 경우")
-        @Test
-        void multipleFailTest() {
-
-            //given
-            int failValue = 0;
-            int testMultiple = calculator.addNumbers();
-            //when
-            //then
-            assertNotEquals(testMultiple, failValue, "값이 일치합니다!");
+        void testInputOverTwo() {
+            assertThrows(RuntimeException.class, () -> calculator.setNumber("1,2,3"));
         }
     }
+
 }
