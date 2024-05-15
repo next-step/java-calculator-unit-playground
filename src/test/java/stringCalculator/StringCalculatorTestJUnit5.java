b1151219fb7919cd897a71196a0a6eeba0c0071b
mlngwan <david027301@naver.com>
2024-05-15 11:28:18 +0900

    Test: 테스트코드 상수 설정, 및 개행 수정, 중첩 테스트 클래스 설정

--- src/test/java/stringCalculator/StringCalculatorTestJUnit5.java
+++ src/test/java/stringCalculator/StringCalculatorTestJUnit5.java
@@ -1,47 +1,54 @@
 package stringCalculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import simpleCalculator.model.StringCalculator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringCalculatorTestJUnit5 {
-    
-    public static class stringTest {
-        
+
+    private static final int EXPECT_VALUE_ZERO = 0;
+    private static final int EXPECT_VALUE_SIX = 6;
+
+    @DisplayName("연산기능 테스트")
+    @Nested
+    class functionTest {
+
         @Test
         @DisplayName("값이 없거나 비어있는 경우")
         public void nullAndEmpty() {
-            
             StringCalculator calculator = new StringCalculator();
-            assertEquals(0, calculator.add(null));
-            assertEquals(0, calculator.add(""));
+            assertEquals(EXPECT_VALUE_ZERO, calculator.add(null));
+            assertEquals(EXPECT_VALUE_ZERO, calculator.add(""));
         }
 
         @Test
         @DisplayName("기본 구분자를 사용하는 경우")
         public void addWithDefault() {
-            
             StringCalculator calculator = new StringCalculator();
-            assertEquals(6, calculator.add("1,2,3"));
-            assertEquals(6, calculator.add("1,2:3"));
-            assertEquals(6, calculator.add("1:2,3"));
-            assertEquals(6, calculator.add("1:2:3"));
+            assertEquals(EXPECT_VALUE_SIX, calculator.add("1,2,3"));
+            assertEquals(EXPECT_VALUE_SIX, calculator.add("1,2:3"));
+            assertEquals(EXPECT_VALUE_SIX, calculator.add("1:2,3"));
+            assertEquals(EXPECT_VALUE_SIX, calculator.add("1:2:3"));
         }
 
         @Test
         @DisplayName("커스텀 구분자를 사용하는 경우")
         public void addWithCustom() {
-            
             StringCalculator calculator = new StringCalculator();
-            assertEquals(6, calculator.add("//;\n1;2;3"));
+            assertEquals(EXPECT_VALUE_SIX, calculator.add("//;\n1;2;3"));
         }
+    }
 
+    @DisplayName("예외처리 테스트")
+    @Nested
+    class exceptionTest {
         @Test
         @DisplayName("숫자가 아닌 다른 값을 입력한 경우")
         public void isNotNumber() {
-            
             StringCalculator calculator = new StringCalculator();
             assertThrows(RuntimeException.class, () -> calculator.add("hi,1,2"));
             assertThrows(RuntimeException.class, () -> calculator.add("hi"));
@@ -50,7 +57,6 @@ public class StringCalculatorTestJUnit5 {
         @Test
         @DisplayName("음수를 입력한 경우")
         public void isNegative() {
-            
             StringCalculator calculator = new StringCalculator();
             assertThrows(RuntimeException.class, () -> calculator.add("-2,1,2"));
             assertThrows(RuntimeException.class, () -> calculator.add("-2"));
