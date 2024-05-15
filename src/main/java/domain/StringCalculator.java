9ae13bb742725adb20b60f4d8fceb36c5c7a7861
Hheojiwon <0618amy@naver.com>
2024-05-15 21:03:24 +0900

    refactor: 문자열 계산기 코드 전체적으로 수정

---
+++ src/main/java/domain/StringCalculator.java
@@ -0,0 +1,66 @@
+package domain;
+
+import java.rmi.NoSuchObjectException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringCalculator {
+    private static final String BASIC_DELIMITER=",|:";
+    private static final String FIRST_STRING="//";
+    private static final String LAST_STRING="\n";
+    private String customDelimiter="";
+    private final String input;
+
+    public StringCalculator(String input){
+        this.input=input;
+    }
+
+    public boolean containCustomSplitStr(){
+        return input.startsWith(FIRST_STRING)&&input.contains(LAST_STRING);
+    }
+
+    private String findCustomSplit(){
+        customDelimiter=input.substring(input.indexOf(FIRST_STRING)+2,input.indexOf(LAST_STRING));
+        return customDelimiter;
+    }
+
+    public List<String> splitCustomSplit(){
+        findCustomSplit();
+        String splitStr=input.substring(input.indexOf(LAST_STRING)+1);
+        return Arrays.asList(splitStr.split(customDelimiter));
+    }
+
+    public List<String> splitBasicSplit(){
+        return Arrays.asList(input.split(BASIC_DELIMITER));
+    }
+
+    public List<Integer> changeStrToInt(List<String> strList){
+        try{
+            List<Integer> numbers=new ArrayList<>();
+            for (String string: strList){
+                numbers.add(Integer.parseInt(string));
+            }
+            return numbers;
+        } catch (NumberFormatException exception){
+            throw new RuntimeException("숫자를 입력해주세요.");
+        }
+    }
+
+    public int getSum(List<Integer> numList){
+        int sum=0;
+        for (int num: numList){
+            if(num<0){
+               throw new RuntimeException("음수를 입력할 수 없습니다.");
+            }
+            sum+=num;
+        }
+        return sum;
+    }
+
+    public void checkBlankInput() throws NoSuchObjectException{
+        if (input.isEmpty()){
+            throw new NoSuchObjectException("공백은 입력할 수 없습니다.");
+        }
+    }
+}
