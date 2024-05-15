4fb6e5c2949557a678181775273f2660535b1019
mlngwan <david027301@naver.com>
2024-05-15 11:53:38 +0900

    Refactor: 사용자의 입력을 받는 inputView EOF처리

--- src/main/java/simpleCalculator/view/InputView.java
+++ src/main/java/simpleCalculator/view/InputView.java
@@ -12,11 +12,17 @@ public class InputView {
 
     public String getSimpleNumber() {
         System.out.println("계산할 내용을 입력해주세요.(숫자,연산자,숫자 형식)");
-        return scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            return scanner.nextLine();
+        }
+        return null;
     }
 
     public String getStringNumber() {
         System.out.println("기본(, :)이나 커스텀 구분자(;)를 가지는 문자열을 입력해주세요. (예: 1,2 => 3, //;\\n1;2;3 => 6 반환)");
-        return scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            return scanner.nextLine();
+        }
+        return null;
     }
 }
