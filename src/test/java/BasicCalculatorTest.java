5453db5421cbeffa0bc04889b0513af8aa39899b
Hheojiwon <0618amy@naver.com>
2024-05-17 04:40:31 +0900

    refactor: 코드 정렬, 테스트 코드 수정

--- src/test/java/BasicCalculatorTest.java
+++ src/test/java/BasicCalculatorTest.java
@@ -5,35 +5,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BasicCalculatorTest {
-    final int a=8;
-    final int b=4;
+
+    final int a = 8;
+    final int b = 4;
+
+    final BasicCalculator basicCalculator = new BasicCalculator(a, b);
+
     @Test
-    void 더하기_테스트(){
-        assertEquals(BasicCalculator.addNum(a,b),a+b);
+    void 더하기_테스트() {
+        assertEquals(basicCalculator.addNum(), a + b);
     }
 
     @Test
-    void 빼기_테스트(){
-        assertEquals(BasicCalculator.subtractNum(a,b),a-b);
+    void 빼기_테스트() {
+        assertEquals(basicCalculator.subtractNum(), a - b);
     }
 
     @Test
-    void 곱하기_테스트(){
-        assertEquals(BasicCalculator.multiplyNum(a,b),a*b);
+    void 곱하기_테스트() {
+        assertEquals(basicCalculator.multiplyNum(), a * b);
     }
 
     @Test
-    void 나누기_테스트(){
-        assertEquals(BasicCalculator.divideNum(a,b),a/b);
+    void 나누기_테스트() {
+        assertEquals(basicCalculator.divideNum(), a / b);
     }
 
     @Test
-    void 분모를_0으로_나눈_경우_예외를_발생시킨다(){
-        final int a=5;
-        final int b=0;
+    void 분모를_0으로_나눈_경우_예외를_발생시킨다() {
+        final int a = 5;
+        final int b = 0;
+        final BasicCalculator basicCalculator = new BasicCalculator(a, b);
 
-        assertThrows(ArithmeticException.class, ()->{
-            BasicCalculator.divideNum(a,b);
-        });
+        assertThrows(ArithmeticException.class, basicCalculator::divideNum);
     }
 }
