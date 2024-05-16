81aed3638f3b234ec18ed8acfe9139236e412538
Hheojiwon <0618amy@naver.com>
2024-05-17 04:48:26 +0900

    refactor: 리스트 네임 수정, 객체에서 로직이 수행되도록 코드 수정

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -4,7 +4,6 @@ import domain.StringCalculator;
 import view.InputView;
 import view.OutputView;
 
-import java.rmi.NoSuchObjectException;
 import java.util.List;
 
 public class CalculatorController {
@@ -12,32 +11,20 @@ public class CalculatorController {
     private final InputView inputView;
     private final OutputView outputView;
 
-    public CalculatorController(InputView inputView, OutputView outputView){
-        this.inputView=inputView;
-        this.outputView=outputView;
+    public CalculatorController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
     }
 
-    public void operate(){
-        String input= inputView.inputString();
-        StringCalculator stringCalculator= new StringCalculator(input);
+    public void operate() {
+        String input = inputView.inputString();
+        StringCalculator stringCalculator = new StringCalculator(input);
 
-        try {
-            stringCalculator.checkBlankInput();
-        } catch (NoSuchObjectException e) {
-            throw new RuntimeException(e);
-        }
+        List<String> strNumber;
+        strNumber = stringCalculator.checkSplit();
 
-        List<String> strings;
-
-        if(stringCalculator.containCustomSplitStr()){
-            strings=stringCalculator.splitCustom();
-        }
-        else{
-            strings=stringCalculator.splitBasic();
-        }
-
-        List<Integer> numbers=stringCalculator.changeStrToInt(strings);
-        int answer=stringCalculator.getSum(numbers);
+        List<Integer> numbers = stringCalculator.changeStrToInt(strNumber);
+        int answer = stringCalculator.getSum(numbers);
         outputView.printAnswer(answer);
     }
 }
