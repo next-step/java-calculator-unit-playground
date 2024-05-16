8476a1aa7da58e0c830169332cb90eda5609ec34
Hheojiwon <0618amy@naver.com>
2024-05-17 04:52:37 +0900

    refactor: 코드 정렬, 메서드 변수로 변경, 캡슐화

--- src/main/java/domain/StringCalculator.java
+++ src/main/java/domain/StringCalculator.java
@@ -7,61 +7,72 @@ import java.util.List;
 
 public class StringCalculator {
 
-    private static final String BASIC_DELIMITER=",|:";
-    private static final String FIRST_STRING="//";
-    private static final String LAST_STRING="\n";
-    private String customDelimiter="";
+    private static final String BASIC_DELIMITER = ",|:";
+    private static final String FIRST_STRING = "//";
+    private static final String LAST_STRING = "\n";
     private final String input;
 
-    public StringCalculator(String input){
-        this.input=input;
+    public StringCalculator(String input) {
+        this.input = input;
     }
 
-    public boolean containCustomSplitStr(){
-        return input.startsWith(FIRST_STRING)&&input.contains(LAST_STRING);
+    public boolean containCustomSplitStr() {
+        try {
+            checkBlankInput();
+            return input.startsWith(FIRST_STRING) && input.contains(LAST_STRING);
+        } catch (NoSuchObjectException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private String findCustomSplit(){
-        customDelimiter=input.substring(input.indexOf(FIRST_STRING)+2,input.indexOf(LAST_STRING));
-        return customDelimiter;
+    private void checkBlankInput() throws NoSuchObjectException {
+        if (input.isEmpty()) {
+            throw new NoSuchObjectException("공백은 입력할 수 없습니다.");
+        }
     }
 
-    public List<String> splitCustom(){
-        findCustomSplit();
-        String splitStr=input.substring(input.indexOf(LAST_STRING)+1);
+    public List<String> checkSplit() {
+        if (containCustomSplitStr()) {
+            return splitCustom();
+        } else {
+            return splitBasic();
+        }
+    }
+
+    private List<String> splitCustom() {
+        String customDelimiter = findCustomSplit();
+        String splitStr = input.substring(input.indexOf(LAST_STRING) + 1);
         return Arrays.asList(splitStr.split(customDelimiter));
     }
 
-    public List<String> splitBasic(){
+    private String findCustomSplit() {
+        return input.substring(input.indexOf(FIRST_STRING) + 2, input.indexOf(LAST_STRING));
+    }
+
+    private List<String> splitBasic() {
         return Arrays.asList(input.split(BASIC_DELIMITER));
     }
 
-    public List<Integer> changeStrToInt(List<String> strList){
-        try{
-            List<Integer> numbers=new ArrayList<>();
-            for (String string: strList){
+    public List<Integer> changeStrToInt(List<String> strList) {
+        try {
+            List<Integer> numbers = new ArrayList<>();
+            for (String string : strList) {
                 numbers.add(Integer.parseInt(string));
             }
             return numbers;
-        } catch (NumberFormatException exception){
+        } catch (NumberFormatException exception) {
             throw new RuntimeException("숫자를 입력해주세요.");
         }
     }
 
-    public int getSum(List<Integer> numList){
-        int sum=0;
-        for (int num: numList){
-            if(num<0){
-               throw new RuntimeException("음수를 입력할 수 없습니다.");
+    public int getSum(List<Integer> numList) {
+        int sum = 0;
+        for (int num : numList) {
+            if (num < 0) {
+                throw new RuntimeException("음수를 입력할 수 없습니다.");
             }
-            sum+=num;
+            sum += num;
         }
         return sum;
-    }
-
-    public void checkBlankInput() throws NoSuchObjectException{
-        if (input.isEmpty()){
-            throw new NoSuchObjectException("공백은 입력할 수 없습니다.");
-        }
     }
 }
