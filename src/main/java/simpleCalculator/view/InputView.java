f1858429b7f614c80602e8ac732d936e996c42f0
mlngwan <david027301@naver.com>
2024-05-23 00:47:06 +0900

    Refactoring: 불필요한 부분 제거로 코드 간결화

--- src/main/java/simpleCalculator/view/InputView.java
+++ src/main/java/simpleCalculator/view/InputView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private Scanner scanner;
+    private final Scanner scanner;
 
     public InputView() {
         this.scanner = new Scanner(System.in);
@@ -12,17 +12,11 @@ public class InputView {
 
     public String getSimpleNumber() {
         System.out.println("계산할 내용을 입력해주세요.(숫자,연산자,숫자 형식)");
-        while (scanner.hasNextLine()) {
-            return scanner.nextLine();
-        }
-        return null;
+        return scanner.nextLine();
     }
 
     public String getStringNumber() {
         System.out.println("기본(, :)이나 커스텀 구분자(;)를 가지는 문자열을 입력해주세요. (예: 1,2 => 3, //;\\n1;2;3 => 6 반환)");
-        while (scanner.hasNextLine()) {
-            return scanner.nextLine();
-        }
-        return null;
+        return scanner.nextLine();
     }
 }
