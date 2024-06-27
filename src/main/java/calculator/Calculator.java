58cbfb43321a57496e98c69da99deab661f1cc56
최준호 <junho5336@gmail.com>
2024-06-27 13:43:13 +0900

    feat: 문자열 계산기 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -17,4 +17,29 @@ public class Calculator {
     public int divide(int a, int b) {
         return a / b;
     }
+
+    public int stringSum(String input) {
+        String target = input;
+        String customSeparator = "";
+        if (target == null) {
+            throw new RuntimeException("잘못된 문자열 형식입니다. input: " + input);
+        }
+        if (input.startsWith("//")) {
+            String[] split = input.split("\n");
+            customSeparator = split[0].substring(2);
+            target = split[1];
+        }
+        String regex = "[\\;\\," + customSeparator + "]";
+        String[] numbers = target.split(regex);
+
+        int sum = 0;
+        for (String number : numbers) {
+            try {
+                sum += Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("잘못된 문자열 형식입니다. input: " + input);
+            }
+        }
+        return sum;
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,32 +1,68 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
-
     private Calculator calculator = new Calculator();
 
     @Test
-    void add() {
-        assertEquals(calculator.add(1, 2), 3);
+    void addTest() {
+        assertEquals(3, calculator.add(1, 2));
     }
 
     @Test
-    void minus() {
-        assertEquals(calculator.minus(2, 1), 1);
+    void minusTest() {
+        assertEquals(1, calculator.minus(2, 1));
     }
 
     @Test
-    void multiple() {
-        assertEquals(calculator.multiply(2, 1), 2);
-        assertEquals(calculator.multiply(2, 4), 8);
+    void multipleTest() {
+        assertEquals(2, calculator.multiply(2, 1));
+        assertEquals(8, calculator.multiply(2, 4));
     }
 
     @Test
-    void divide() {
-        assertEquals(calculator.divide(8, 4), 2);
+    void divideTest() {
+        assertEquals(2, calculator.divide(8, 4));
+    }
+
+    @Test
+    void stringSumTest() {
+        assertEquals(15, calculator.stringSum("1;2;3;4,5"));
+    }
+
+    @Test
+    void stringSumTestWithCustomSeq() {
+        assertEquals(11, calculator.stringSum("//y\n1y5y1y1y1y2"));
+    }
+
+    @Nested
+    class withWrongString {
+
+        @Test
+        void stringSumTestWithWrongString() {
+            assertThrows(RuntimeException.class, () -> {
+                calculator.stringSum("//y\n1t2t3");
+            });
+        }
+
+        @Test
+        void stringSumTestWithWrongString2() {
+            assertThrows(RuntimeException.class, () -> {
+                calculator.stringSum("/y\n1y2y3");
+            });
+        }
+
+        @Test
+        void stringSumTestWithWrongString3() {
+            assertThrows(RuntimeException.class, () -> {
+                calculator.stringSum("1,2,3,44:5");
+            });
+        }
     }
 }
