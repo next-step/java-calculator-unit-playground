2930adcf95ed6d32ced8749edfd7cd6e2c90c687
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-06-28 22:53:54 +0900

    1단계 - 초간단 계산기 구현

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    int add(int i, int j){
+        return i + j;
+    }
+
+    int substract(int i, int j){
+        return i - j;
+    }
+
+    int multiple(int i, int j){
+        return i * j;
+    }
+
+    int divide(int i, int j){
+        return i / j;
+    }
+}
