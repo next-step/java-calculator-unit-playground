2b55d8282e6a20e8fb94d806b379fcddd86015de
최준호 <junho5336@gmail.com>
2024-06-29 10:52:29 +0900

    refactor: 리뷰반영

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -29,13 +29,17 @@ public class Calculator {
             customSeparator = split[0].substring(2);
             target = split[1];
         }
-        String regex = "[\\:\\," + customSeparator + "]";
+        String regex = "[:," + customSeparator + "]";
         String[] numbers = target.split(regex);
 
         int sum = 0;
         for (String number : numbers) {
             try {
-                sum += Integer.parseInt(number);
+                int parsed = Integer.parseInt(number);
+                if (parsed < 0) {
+                    throw new RuntimeException("해당 계산에는 음수가 포함될 수 없습니다");
+                }
+                sum += parsed;
             } catch (NumberFormatException e) {
                 throw new RuntimeException("잘못된 문자열 형식입니다. input: " + input);
             }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,48 +4,83 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
 class CalculatorTest {
 
     private final Calculator calculator = new Calculator();
 
-    @Test
-    void addTest() {
-        assertThat(calculator.add(1, 2)).isEqualTo(3);
-    }
+    @Nested
+    class 더하기_연산을_수행한다 {
 
-    @Test
-    void minusTest() {
-        assertThat(calculator.minus(2, 1)).isEqualTo(1);
-    }
-
-    @Test
-    void multipleTest() {
-        assertThat(calculator.multiply(2, 1)).isEqualTo(2);
-        assertThat(calculator.multiply(2, 4)).isEqualTo(8);
-    }
-
-    @Test
-    void divideTest() {
-        assertThat(calculator.divide(8, 4)).isEqualTo(2);
-    }
-
-    @Test
-    void stringSumTest() {
-        assertEquals(15, calculator.stringSum("1:2:3:4,5"));
-    }
-
-    @Test
-    void stringSumTestWithCustomSeq() {
-        assertEquals(11, calculator.stringSum("//y\n1y5,1y1y1y2"));
+        @ParameterizedTest(name = "{0} + {1} = {2}")
+        @CsvSource(value = {"1,2,3", "3,4,7", "100000,10000,110000"}, delimiter = ',')
+        void addTest(int a, int b, int result) {
+            assertThat(calculator.add(a, b)).isEqualTo(result);
+        }
     }
 
     @Nested
-    class withWrongString {
+    class 빼기_연산을_수행한다 {
+
+        @ParameterizedTest(name = "{0} - {1} = {2}")
+        @CsvSource(value = {"2,1,1", "1,2,-1", "1234,1234,0"}, delimiter = ',')
+        void addTest(int a, int b, int result) {
+            assertThat(calculator.minus(a, b)).isEqualTo(result);
+        }
+    }
+
+    @Nested
+    class 곱하기_연산을_수행한다 {
+
+        @ParameterizedTest(name = "{0} x {1} = {2}")
+        @CsvSource(value = {"2,1,2", "1,-42,-42", "1234,2,2468"}, delimiter = ',')
+        void multiplyTest(int a, int b, int result) {
+            assertThat(calculator.multiply(a, b)).isEqualTo(result);
+        }
+    }
+
+    @Nested
+    class 나누기_연산을_수행한다 {
+
+        @ParameterizedTest(name = "{0} / {1} = {2}")
+        @CsvSource(value = {"2,1,2", "6,2,3", "2468,2,1234"}, delimiter = ',')
+        void divideTest(int a, int b, int result) {
+            assertThat(calculator.divide(a, b)).isEqualTo(result);
+        }
+
+        @ParameterizedTest(name = "0으로 나누면 예외: {0} / {1} = {2}")
+        @CsvSource(value = {"2,0,1", "6,0,2", "2468,0,2"}, delimiter = ',')
+        void divideZeroTest(int a, int b) {
+            assertThrows(RuntimeException.class, () -> calculator.divide(a, b));
+        }
+    }
+
+    @Nested
+    class 문자열_더하기를_수행한다 {
 
         @Test
+        @DisplayName("기본 구분자는 :과 ,이다")
+        void default_delimiter() {
+            assertEquals(15, calculator.stringSum("1:2:3:4,5"));
+        }
+
+        @Test
+        @DisplayName("임의의 구분자를 지정할 수 있다.")
+        void stringSumTestWithCustomSeq() {
+            assertEquals(11, calculator.stringSum("//y\n1y5,1y1y1y2"));
+        }
+
+        @Test
+        @DisplayName("구분자 외의 문자가 입력되면 예외")
         void stringSumTestWithWrongString() {
             assertThrows(RuntimeException.class, () -> {
                 calculator.stringSum("//y\n1t2t3");
@@ -53,16 +88,10 @@ class CalculatorTest {
         }
 
         @Test
-        void stringSumTestWithWrongString2() {
+        @DisplayName("음수가 입력되면 예외")
+        void stringSumTestWithNegative() {
             assertThrows(RuntimeException.class, () -> {
-                calculator.stringSum("/y\n1y2y3");
-            });
-        }
-
-        @Test
-        void stringSumTestWithWrongString3() {
-            assertThrows(RuntimeException.class, () -> {
-                calculator.stringSum("1,2,3,44;5");
+                calculator.stringSum("//y\n1y-2y3");
             });
         }
     }
