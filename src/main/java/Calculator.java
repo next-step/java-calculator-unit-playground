fd0d41a3754fe3bcef950f0597f329fffbae94c3
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-07-01 22:07:54 +0900

    3단계 - 문자열 계산기 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,46 @@
+import java.util.Arrays;
+
 public class Calculator {
-    int add(int i, int j){
+
+    int add(int i, int j) {
         return i + j;
     }
 
-    int substract(int i, int j){
+    int subtract(int i, int j) {
         return i - j;
     }
 
-    int multiple(int i, int j){
+    int multiple(int i, int j) {
         return i * j;
     }
 
-    int divide(int i, int j){
+    int divide(int i, int j) {
         return i / j;
+    }
+
+    int stringAdd(String input) {
+        String customSeparator = "";
+        String[] expressions = {};
+
+        if (input.startsWith("//")) {
+            expressions = input.split("\n");
+            customSeparator = expressions[0].substring(2);
+        }
+        String[] expression = expressions[1].split("[,|:|" + customSeparator + "]");
+
+        int result = 0;
+        for (String a : expression) {
+            try {
+                int num = Integer.parseInt(a);
+                if (num < 0) {
+                    throw new RuntimeException();
+                }
+                result += num;
+            } catch (Exception e) {
+                throw new RuntimeException("잘못된 문자열입니다.");
+            }
+        }
+
+        return result;
     }
 }
