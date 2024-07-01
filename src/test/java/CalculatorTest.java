48c6f5f109685b20080339c1ba46d4d7b04432c5
dradnats1012 <herjebi1012@gmail.com>
2024-07-01 18:52:34 +0900

    feat: 4단계 미션 구현

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,5 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -12,43 +14,45 @@ class CalculatorTest {
     @Test
     @DisplayName("더하기 기능을 테스트한다")
     void addTest() {
-        assertEquals(8, calculator.add(5, 3));
+        assertThat(calculator.add(5, 3)).isEqualTo(8);
     }
 
     @Test
     @DisplayName("빼기 기능을 테스트한다")
     void minusTest() {
-        assertEquals(2, calculator.minus(5, 3));
+        assertThat(calculator.minus(5, 3)).isEqualTo(2);
     }
 
     @Test
     @DisplayName("곱하기 기능을 테스트한다")
     void multipleTest() {
-        assertEquals(15, calculator.multiple(5, 3));
+        assertThat(calculator.multiple(5, 3)).isEqualTo(15);
     }
 
     @Test
     @DisplayName("나누기 기능을 테스트한다")
     void divideTest() {
-        assertEquals(1, calculator.divide(5, 3));
+        assertThat(calculator.divide(5, 3)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("문자열 더하기 기능을 테스트한다")
     void stringAddTest() {
-        assertEquals(8, calculator.stringAdd("5,3"));
+        assertThat(calculator.stringAdd("5,3")).isEqualTo(8);
     }
 
     @Test
     @DisplayName("문자열 커스텀더하기 기능을 테스트한다")
     void stringCustomAddTest() {
-        assertEquals(8, calculator.stringAdd("//;\n5;3"));
+        assertThat(calculator.stringAdd("//;\n5;3")).isEqualTo(8);
     }
 
     @Test
     @DisplayName("음수가 있으면 RuntimeExcrption")
     void negativeRuntimeException() {
-        assertThrows(RuntimeException.class, this::addNegative);
+        assertThatThrownBy(() -> {
+            addNegative();
+        } ).isInstanceOf(RuntimeException.class);
     }
 
     private void addNegative() {
@@ -58,6 +62,6 @@ class CalculatorTest {
     @Test
     @DisplayName("빈 문자열은 0")
     void emptyString() {
-        assertEquals(0, calculator.stringAdd(""));
+        assertThat( calculator.stringAdd("")).isZero();
     }
 }
