c9574031b322f6f3b47675593fb55482b2b64bc9
LHC0312 <chan050312@gmail.com>
2024-09-23 12:51:49 +0900

    feat: 사칙연산 추가

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+  public int add(int num1, int num2) {
+    return num1+num2;
+  }
+
+  public int sub(int num1, int num2) {
+    return num1-num2;
+  }
+  public int divis(int num1, int num2) {
+    return num1/num2;
+  }
+  public int multi(int num1, int num2) {
+    return num1*num2;
+  }
+
+}
