e820747a9634fff333867d2a3ce014a6995eceff
StoneCAU <seokwoo1999@naver.com>
2024-09-25 01:00:57 +0900

    feat: 커스텀 구분자 구현

--- src/main/java/StringCalculator/domain/StringCalculator.java
+++ src/main/java/StringCalculator/domain/StringCalculator.java
@@ -1,6 +1,5 @@
 package StringCalculator.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static StringCalculator.util.Config.*;
@@ -18,8 +17,29 @@ public class StringCalculator {
     }
 
     private static void parsing(String input) {
-        if(customExist()) operations = List.of(input.split(format("[%s|%s|%s]", FIRST_SEPARATOR, SECOND_SEPARATOR, CUSTOM_SEPARATOR)));
+        settingCustomSeparator(input);
+
+        if(customExist()) {
+            input = input.substring(FIRST_CUSTOM_SETTING_STRING.length()
+                    + CUSTOM_SEPARATOR.length() + SECOND_CUSTOM_SETTING_STRING.length());
+            operations = List.of(input.split(format("[%s|%s|%s]", FIRST_SEPARATOR, SECOND_SEPARATOR, CUSTOM_SEPARATOR)));
+        }
         else operations = List.of(input.split(format("[%s|%s]", FIRST_SEPARATOR, SECOND_SEPARATOR)));
+    }
+
+    private static void settingCustomSeparator(String input) {
+        if(input.startsWith(FIRST_CUSTOM_SETTING_STRING)) {
+            input = input.substring(FIRST_CUSTOM_SETTING_STRING.length());
+
+            int index = input.indexOf(SECOND_CUSTOM_SETTING_STRING);
+
+            if(index != -1) {
+                String result = input.substring(0, index);
+
+                if(result != "") CUSTOM_SEPARATOR = result;
+            }
+
+        }
     }
 
     private static boolean customExist() {
