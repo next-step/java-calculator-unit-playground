c523349d0012fb76f0d43155dd75f4c26e37cb21
StoneCAU <seokwoo1999@naver.com>
2024-09-25 00:57:22 +0900

    refactor: CUSTOM_SEPARATOR 변수 final 제거

--- src/main/java/StringCalculator/util/Config.java
+++ src/main/java/StringCalculator/util/Config.java
@@ -3,7 +3,7 @@ package StringCalculator.util;
 public class Config {
     final public static String FIRST_SEPARATOR = ",";
     final public static String SECOND_SEPARATOR = ";";
-    final public static String CUSTOM_SEPARATOR = "";
+    public static String CUSTOM_SEPARATOR = "";
     final public static String FIRST_CUSTOM_SETTING_STRING = "//";
     final public static String SECOND_CUSTOM_SETTING_STRING = "\\n";
 }
