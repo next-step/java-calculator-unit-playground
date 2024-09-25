2106b7cdc7cdd3d846261103f4b51c00cbddd359
bingle625 <fhqhxm8899@gmail.com>
2024-09-26 00:06:50 +0900

    test: 문자열 계산기 실패하는 테스트 작성

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -9,12 +9,32 @@ import org.junit.jupiter.api.Test;
 public class StringCalculatorTest {
 
   @Test
-  @DisplayName("덧셈_테스트")
-  void testAddition() {
+  @DisplayName("덧셈_테스트_케이스_1")
+  void testAdditionCase1() {
     String targetText = "1,2,3";
 
     int result = StringCalculator.parseAndSum(targetText);
 
     assertEquals(6, result);
+  }
+
+  @Test
+  @DisplayName("덧셈_테스트_케이스_2")
+  void testAdditionCase2() {
+    String targetText = "1,2";
+
+    int result = StringCalculator.parseAndSum(targetText);
+
+    assertEquals(3, result);
+  }
+
+  @Test
+  @DisplayName("덧셈_테스트_케이스_구분자가_다른_경우")
+  void testAdditionCaseDifferentSeparator() {
+    String targetText = "//;\\n1;2;4";
+
+    int result = StringCalculator.parseAndSum(targetText);
+
+    assertEquals(7, result);
   }
 }
