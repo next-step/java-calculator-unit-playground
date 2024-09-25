12618218044371b3fdefb3cda42f1d6ec6ce6aab
bingle625 <fhqhxm8899@gmail.com>
2024-09-25 23:00:04 +0900

    feat: 사칙연산을 수행하는 Calculator 클래스 구현

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+  public static int add(final int a, final int b) {
+    return a + b;
+  }
+
+  public static int subtractBFromA(final int a, final int b) {
+    return a - b;
+  }
+
+  public static int multiply(final int a, final int b) {
+    return a * b;
+  }
+
+  public static int divideAByB(final int a, final int b) {
+    return a / b;
+  }
+}
