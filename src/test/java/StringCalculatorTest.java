c531303d83db12b2026f319f7c0c8735c07441ee
csh3424 <csh3424@icloud.com>
2024-09-28 00:57:41 +0900

    Test: 문자열 계산기 예외 처리 테스트 추가

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorTest_JUnit5 {
 
@@ -23,6 +25,25 @@ class StringCalculatorTest_JUnit5 {
     String expression = "//;\n3;2;5";
 
     assertEquals(sc.sum(expression), result);
+  }
+
+  @Test
+  @DisplayName("음수가_들어오면_오류를_반환한다")
+  void 음수가_들어오면_오류를_반환한다() {
+    String expression = "-1,3:6";
+
+    assertThrows(RuntimeException.class, () -> {
+      sc.sum(expression);
+    });
+  }
+  @Test
+  @DisplayName("숫자가_아니면_오류를_반환한다")
+  void 숫자가_아니면_오류를_반환한다() {
+    String expression = "//;\n3;&;5";
+
+    assertThrows(RuntimeException.class, () -> {
+      sc.sum(expression);
+    });
   }
 }
 
@@ -47,5 +68,25 @@ class StringCalculatorTest_AssertJ {
 
     assertThat(sc.sum(expression))
         .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("음수가_들어오면_오류를_반환한다")
+  void 음수가_들어오면_오류를_반환한다() {
+    String expression = "1,-3:6";
+
+    assertThatThrownBy(()->{
+      sc.sum(expression);
+    }).isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  @DisplayName("숫자가_아니면_오류를_반환한다")
+  void 숫자가_아니면_오류를_반환한다() {
+    String expression = "//;\n3;2;p";
+
+    assertThatThrownBy(()->{
+      sc.sum(expression);
+    }).isInstanceOf(RuntimeException.class);
   }
 }
