1bdabb546ac32e09d361a8a4adee808affc2b1da
csh3424 <csh3424@icloud.com>
2024-09-27 23:57:06 +0900

    Test: 문자열 계산기 AssertJ 테스트 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,8 +2,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class CalculatorTest {
+class CalculatorTest_JUnit5 {
 
   Calculator calculator = new Calculator();
   final int result = 10;
@@ -78,5 +79,92 @@ class CalculatorTest {
     double b = 6.0;
 
     assertEquals(calculator.div(a, b), result);
+  }
+}
+
+class CalculatorTest_AssertJ {
+
+  Calculator calculator = new Calculator();
+  final int result = 10;
+
+  //  JUnit5Test
+  @Test
+  @DisplayName("정수_합_연산을_실행한다")
+  void 정수_합_연산을_실행한다() {
+    int a = 4;
+    int b = 6;
+
+    assertThat(calculator.sum(a, b))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("실수_합_연산을_실행한다")
+  void 실수_합_연산을_실행한다() {
+    double a = 3.5;
+    double b = 6.5;
+
+    assertThat(calculator.sum(a, b))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("정수_차_연산을_실행한다")
+  void 정수_차_연산을_실행한다() {
+    int a = 15;
+    int b = 5;
+
+    assertThat(calculator.sub(a, b))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("실수_차_연산을_실행한다")
+  void 실수_차_연산을_실행한다() {
+    double a = 15.5;
+    double b = 5.5;
+
+    assertThat(calculator.sub(a, b))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("정수_곱_연산을_실행한다")
+  void 정수_곱_연산을_실행한다() {
+    int a = 2;
+    int b = 5;
+
+    assertThat(calculator.mul(a, b))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("실수_곱_연산을_실행한다")
+  void 실수_곱_연산을_실행한다() {
+    double a = 2.0;
+    double b = 5.0;
+
+    assertThat(calculator.mul(a, b))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("정수_몫_연산을_실행한다")
+  void 정수_몫_연산을_실행한다() {
+    int a = 50;
+    int b = 5;
+
+    assertThat(calculator.div(a, b))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("실수_몫_연산을_실행한다")
+  void 실수_몫_연산을_실행한다() {
+    double a = 60.0;
+    double b = 6.0;
+
+    assertThat(calculator.div(a, b))
+        .isEqualTo(result);
   }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,8 +2,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class StringCalculatorTest {
+class StringCalculatorTest_JUnit5 {
 
   StringCalculator sc = new StringCalculator();
   final int result = 10;
@@ -15,11 +16,36 @@ public class StringCalculatorTest {
 
     assertEquals(sc.sum(expression), result);
   }
+
   @Test
   @DisplayName("커스텀_구분자로_문자열_계산기를_실행한다")
   void 커스텀_구분자로_문자열_계산기를_실행한다() {
     String expression = "//;\n3;2;5";
 
     assertEquals(sc.sum(expression), result);
+  }
+}
+
+class StringCalculatorTest_AssertJ {
+
+  StringCalculator sc = new StringCalculator();
+  final int result = 10;
+
+  @Test
+  @DisplayName("기본_구분자로_문자열_계산기를_실행한다")
+  void 기본_구분자로_문자열_계산기를_실행한다() {
+    String expression = "1,3:6";
+
+    assertThat(sc.sum(expression))
+        .isEqualTo(result);
+  }
+
+  @Test
+  @DisplayName("커스텀_구분자로_문자열_계산기를_실행한다")
+  void 커스텀_구분자로_문자열_계산기를_실행한다() {
+    String expression = "//;\n3;2;5";
+
+    assertThat(sc.sum(expression))
+        .isEqualTo(result);
   }
 }
