04bfb20fb506eff9066b9b07e107e6910613e81e
bingle625 <fhqhxm8899@gmail.com>
2024-09-27 23:14:44 +0900

    refactor: replace 메서드 체이닝

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -31,10 +31,31 @@ public class StringCalculatorTest {
   @Test
   @DisplayName("덧셈_테스트_케이스_구분자가_다른_경우")
   void testAdditionCaseDifferentSeparator() {
-    String targetText = "//;\n1;2;4";
+    String targetText = "//;\n1;2;4,4:5";
 
     int result = StringCalculator.parseAndSum(targetText);
 
-    assertEquals(7, result);
+    assertEquals(16, result);
+  }
+
+  @Test
+  @DisplayName("덧셈_테스트_케이스_자리수가_여러개인_경우")
+  void testAdditionCaseMultipleDigits() {
+    String targetText = "//;\n12;24;0";
+
+    int result = StringCalculator.parseAndSum(targetText);
+
+    assertEquals(36, result);
+  }
+
+  @Test
+  @DisplayName("덧셈_예외_테스트_케이스_음수인_경우")
+  void testAdditionCaseNegativeNumbers() {
+    String targetText = "//;\n12;-24;0,-13:123";
+
+    assertThrows(RuntimeException.class, ()->{
+      StringCalculator.parseAndSum(targetText);
+    });
+
   }
 }
