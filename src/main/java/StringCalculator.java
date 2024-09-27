a2fd697b1f0631d36196d0fe90ba6235f84a5929
csh3424 <csh3424@icloud.com>
2024-09-28 00:56:56 +0900

    Feat: 문자열 계산기 예외 처리 추가

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -11,31 +11,34 @@ public class StringCalculator {
       String expression = input.substring(endIndex+1);
 
       String[] tokens = expression.split(separator);
-      int token_len = tokens.length;
 
-      return str_sum(token_len, tokens);
+      return str_sum(tokens);
     }
     String[] tokens = input.split("[,|:]");
-    int token_len = tokens.length;
 
-    return str_sum(token_len, tokens);
+    return str_sum(tokens);
   }
 
-  private int str_sum(int token_len, String[] tokens) {
+  private int str_sum(String[] tokens) {
     int sum = 0;
 
-    if(token_len == 1)
-      return Integer.parseInt(tokens[0]);
+    for(String token : tokens) {
+      int number = parseAndValidateNumber(token);
 
-    for(int i=0; i< token_len - 1; i += 2)
-    {
-      int a = Integer.parseInt(tokens[i]);
-      int b = Integer.parseInt(tokens[i+1]);
-      sum += calculator.sum(a, b);
+      if(number < 0)
+        throw new RuntimeException();
+
+      sum += number;
     }
-    if(token_len % 2 == 1)
-      sum += Integer.parseInt(tokens[token_len-1]);
 
     return sum;
+  }
+
+  private int parseAndValidateNumber(String input) {
+    try {
+      return Integer.parseInt(input);
+    } catch(NumberFormatException e) {
+      throw new RuntimeException();
+    }
   }
 }
