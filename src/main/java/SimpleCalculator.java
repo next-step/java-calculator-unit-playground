223d6e11f5da3920f6d515aff6c29aaed2ae5893
GY102912 <qaz102912@gmail.com>
2024-09-27 20:34:54 +0900

    feat: simple calculator

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,20 @@
+public class SimpleCalculator {
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int subtract(int a, int b) {
+    return a - b;
+  }
+
+  public int multiply(int a, int b) {
+    return a * b;
+  }
+
+  public int divide(int a, int b) {
+    if (b == 0) {
+      throw new ArithmeticException("Divide by zero");
+    }
+    return a / b;
+  }
+}
