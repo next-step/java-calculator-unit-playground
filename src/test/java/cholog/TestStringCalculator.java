1535235817f3285e202fcb47dd9ef4ec8a3931fe
GY102912 <qaz102912@gmail.com>
2024-09-27 23:34:59 +0900

    test: add runtimeexceiption test for string calculation

--- src/test/java/cholog/TestStringCalculator.java
+++ src/test/java/cholog/TestStringCalculator.java
@@ -46,7 +46,7 @@ public class TestStringCalculator {
   @DisplayName("지정되지 않은 구분자 에러 확인")
   public void unknownSeperator() {
     //given
-    String input = "1;2;3";
+    String input = "1%2%3";
 
     //when
 
@@ -54,4 +54,29 @@ public class TestStringCalculator {
     //then
     assertThatThrownBy(() -> calculator.sum(input)).isInstanceOf(Throwable.class);
   }
+  @Test
+  @DisplayName("음수 입력 에러 확인")
+  public void negative() {
+    //given
+    String input = "1;-2;3";
+
+    //when
+
+
+    //then
+    assertThatThrownBy(() -> calculator.sum(input)).isInstanceOf(RuntimeException.class);
+  }
+  @Test
+  @DisplayName("숫자가 아닌 문자 입력 에러 확인")
+  public void isNotNumber() {
+    //given  private static final Pattern isNumber = Pattern.compile("^[0-9]*$");
+    String input = "1;~~;3";
+
+    //when
+
+
+    //then  private static final Pattern isNumber = Pattern.compile("^[0-9]*$");
+    assertThatThrownBy(() -> calculator.sum(input)).isInstanceOf(RuntimeException.class);
+  }
+
 }
