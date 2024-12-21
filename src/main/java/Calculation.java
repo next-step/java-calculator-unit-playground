cf9dabe845ba8f8b9bd68d5aa571b1dc1654f6c1
Rulu <hafnium1923@gmail.com>
2024-12-21 21:34:10 +0900

    feat: 문자열 계산기 구현

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -18,4 +18,30 @@ public class Calculation {
     double division(double a, double b) {
         return a / b;
     }
+
+    double stringPlusCalc(String input) {
+        String delimiter = ",|:";
+
+        if (input.startsWith("//")) {
+            int endIndex = input.indexOf("\n");
+            if (endIndex == -1) {
+                throw new RuntimeException("커스텀 구분자 등록이 실패했습니다.");
+            }
+            delimiter += "|" + input.substring(2, endIndex);
+            input = input.substring(endIndex + 1);
+        }
+
+        String[] items = input.split(delimiter);
+        double sum = 0;
+        for (String item : items) {
+            try {
+                double number = Double.parseDouble(item.trim());
+                sum += number;
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("숫자와 구분자만 입력하세요");
+            }
+        }
+
+        return sum;
+    }
 }
--- src/test/java/JUnit5Test.java
+++ src/test/java/JUnit5Test.java
@@ -113,5 +113,53 @@ public class JUnit5Test {
                 throw new RuntimeException(calc.multiplication(0.1, 0.2) + "소숫점 곱셈 실패");
             }
         }
+
+        @Test
+        @DisplayName("문자열 덧셈 테스트")
+        void Test_stringPlusCalc() {
+            Calculation calc = new Calculation();
+
+            RuntimeException exception = assertThrows(RuntimeException.class,
+                    () -> calc.stringPlusCalc(""),
+                    "빈문자열은 RuntimeException을 발생시켜야 합니다.");
+            assertEquals("숫자와 구분자만 입력하세요", exception.getMessage(), "예외 메시지 확인.");
+
+            if (calc.stringPlusCalc("1,2") == 3) {
+                System.out.println("쉼표 구분자 성공");
+            } else {
+                throw new RuntimeException(calc.stringPlusCalc("1,2") + "쉼표 구분자 실패");
+            }
+
+            if (calc.stringPlusCalc("1:2:3") == 6) {
+                System.out.println("콜론 구분자 성공");
+            } else {
+                throw new RuntimeException(calc.stringPlusCalc("1:2:3") + "콜론 구분자 실패");
+            }
+
+            if (calc.stringPlusCalc("1,2:3,4") == 10) {
+                System.out.println("기본 구분자 혼합 성공");
+            } else {
+                throw new RuntimeException(calc.stringPlusCalc("1,2:3,4") + "기본 구분자 혼합 실패");
+            }
+
+            if (calc.stringPlusCalc("//;\n1;2;3") == 6) {
+                System.out.println("커스텀 구분자 성공");
+            } else {
+                throw new RuntimeException(calc.stringPlusCalc("//;\n1;2;3") + "커스텀 구분자 실패");
+            }
+
+            if (calc.stringPlusCalc("//-\n1-2-3,4:5") == 15) {
+                System.out.println("커스텀 구분자와 기본 구분자 혼합 성공");
+            } else {
+                throw new RuntimeException(calc.stringPlusCalc("//-\n1-2-3,4:5") + "커스텀 구분자와 기본 구분자 혼합 실패");
+            }
+
+            RuntimeException exception2 = assertThrows(RuntimeException.class,
+                    () -> calc.stringPlusCalc("1,abc,3"),
+                    "구분자, 숫자만 들어가야합니다.");
+            assertEquals("숫자와 구분자만 입력하세요", exception.getMessage(), "예외 메시지 확인.");
+
+          
+        }
     }
 }
