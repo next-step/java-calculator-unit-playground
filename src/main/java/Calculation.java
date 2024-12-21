0f9a3ccd71a29064f0662c9855a7b39f2aa93ff5
Rulu <hafnium1923@gmail.com>
2024-12-21 20:40:55 +0900

    feat: 인자 2개로 사칙연산 함수

---
+++ src/main/java/Calculation.java
@@ -0,0 +1,17 @@
+public class Calculation {
+    double plus(double a, double b) {
+        return a + b;
+    }
+
+    double minus(double a, double b) {
+        return a - b;
+    }
+    double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    double division(double a, double b) {
+        return a / b;
+    }
+
+}
