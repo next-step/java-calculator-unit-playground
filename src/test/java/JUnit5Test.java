6e84f27a372f7b14832b427eb1e4ffe751aa6cdf
jihyun-Yun42 <jyun9842@gmail.com>
2024-12-22 16:49:39 +0900

    feat: JUnit5 test code

---
+++ src/test/java/JUnit5Test.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("JUnit5 테스트")
+public class JUnit5Test {
+    @Nested
+    @DisplayName("@Test 계산기 테스트")
+    class calculatorTest {
+        Calculator calculator = new Calculator();
+        
+        @Test
+        @DisplayName("더하기 테스트")
+        void add_test() {
+            assertEquals(calculator.add(1, 2), 3, "더하기 테스트 실패");
+        }
+
+        @Test
+        @DisplayName("빼기 테스트")
+        void subtract_test() {
+            assertEquals(calculator.subtract(4, 2), 2, "빼기 테스트 실패");
+        }
+        
+        @Test
+        @DisplayName("곱하기 테스트")
+        void multiply_test() {
+            assertEquals(calculator.multiply(4, 2), 8, "곱하기 테스트 실패");
+        }
+        
+        @Test
+        @DisplayName("나누기 테스트")
+        void divide_test() {
+            assertEquals(calculator.divide(10, 2), 5, "나누기 테스트 실패");
+        }
+
+
+    }
+}
