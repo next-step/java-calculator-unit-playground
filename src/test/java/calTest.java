3482797fcd29ec975a0555ea210e63985c49ffec
jaehan0524 <shc040524@gmail.com>
2025-01-20 14:02:59 +0900

    이런식으로 과제를 진행하는게 맞는지 궁금합니다

--- src/test/java/calTest.java
+++ src/test/java/calTest.java
@@ -1,9 +1,61 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class calTest {
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(10, 20));
-        System.out.println(calc.sub(10, 20));
-        System.out.println(calc.mul(10, 20));
-        System.out.println(calc.div(20, 10));
+
+    @Nested
+    @DisplayName("계산기 사칙연산 테스트")
+    class CalTests {
+
+        @Test
+        void AddTest() {
+            Calculator calc = new Calculator();
+            assertEquals(calc.add(2, 3), 5);
+        }
+        @Test
+        void AddWrongTest() {
+            Calculator calc = new Calculator();
+            assertEquals( calc.add(2, 3), 6);
+        }
+
+        @Test
+        void SubTest() {
+            Calculator calc = new Calculator();
+            assertEquals(calc.sub(5, 1), 4);
+        }
+
+        @Test
+        void SubWrongTest() {
+            Calculator calc = new Calculator();
+            assertEquals(calc.sub(5, 1), 5);
+        }
+
+        @Test
+        void MulTest() {
+            Calculator calc = new Calculator();
+            assertEquals(calc.mul(2, 3), 6);
+        }
+
+        @Test
+        void MulWrongTest() {
+            Calculator calc = new Calculator();
+            assertEquals(calc.mul(2, 3), 5);
+        }
+
+        @Test
+        void DivTest() {
+            Calculator calc = new Calculator();
+            assertEquals(calc.div(6, 3), 2);
+        }
+
+        @Test
+        void DivWrongTest() {
+            Calculator calc = new Calculator();
+            assertEquals( calc.div(6, 3), 1);
+        }
+
     }
 }
