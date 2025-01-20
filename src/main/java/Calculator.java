947b889e2ef2648e21e91e983c5391a815ef01c9
Kangdora <ioprt123@gmail.com>
2025-01-20 14:10:52 +0900

    feat: 사칙연산 기능 구현

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static int add(int n, int m){
+        return n + m;
+    }
+
+    public static int subtract(int n, int m){
+        return n - m;
+    }
+
+    public static int multiply(int n, int m){
+        return n * m;
+    }
+
+    public static int division(int n, int m){
+        return n / m;
+    }
+}
---
+++ src/test/java/JUnit5Calculator.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class JUnit5Test {
+}
