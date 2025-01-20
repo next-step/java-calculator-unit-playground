876dfcc79cb13d1d86248a6a00f575bc7f312ac6
jaehan0524 <shc040524@gmail.com>
2025-01-20 13:02:19 +0900

    이런식으로 과제를 진행하는게 맞는지 궁금합니다

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/calTest.java
@@ -0,0 +1,9 @@
+public class calTest {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(10, 20));
+        System.out.println(calc.sub(10, 20));
+        System.out.println(calc.mul(10, 20));
+        System.out.println(calc.div(20, 10));
+    }
+}
