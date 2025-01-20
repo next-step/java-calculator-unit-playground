5267f1d58a16a5ef2946b9938c0c95b763232f04
nns503 <73779581+nns503@users.noreply.github.com>
2025-01-20 23:14:25 +0900

    feat: 사칙연산 메서드 구현

---
+++ src/main/java/Calculate.java
@@ -0,0 +1,18 @@
+public class Calculate {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
