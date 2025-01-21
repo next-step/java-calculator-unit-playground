e8e2bcf60f9e1820e3d4e69c8b19b700efdc0e83
dldb-chamchi <123imaricom@gmail.com>
2025-01-21 15:58:15 +0900

    간단 계산기 클래스 구현

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -6,24 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SimpleCalculatorTest {
     @Nested
     class SimpleSimpleCalculatorTest {
+
         @Test
         void addTest(){
             SimpleCalculator calc = new SimpleCalculator();
             assertEquals(3, calc.add(1, 2));
             assertEquals(5, calc.add(3, 2));
         }
+
         @Test
         void minusTest(){
             SimpleCalculator calc = new SimpleCalculator();
             assertEquals(3, calc.minus(5, 2));
             assertEquals(-2, calc.minus(3, 5));
         }
+
         @Test
         void multiply(){
             SimpleCalculator calc = new SimpleCalculator();
             assertEquals(10, calc.multiply(5, 2));
             assertEquals(15, calc.multiply(3, 5));
         }
+
         @Test
         void divide(){
             SimpleCalculator calc = new SimpleCalculator();
