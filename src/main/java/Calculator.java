5b1484de9af9b2f8e4544c295ade94d2414e1da2
minjun-oh <a39229121@gmail.com>
2025-01-21 21:33:48 +0900

    step1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    private int result = 0;
+    public int add(String nums) {
+        if (nums == null || nums.isEmpty()) return 0;
+
+//        result = ;
+        return result;
+    }
+}
--- src/main/java/Calculator_Simple.java
+++ src/main/java/Calculator_Simple.java
@@ -1,21 +1,21 @@
 public class Calculator_Simple {
-    private static int result = 0;
-    public static int add(int a, int b) {
+    private int result = 0;
+    public int add(int a, int b) {
         result = a + b;
         return result;
     }
 
-    public static int sub(int a, int b) {
+    public int sub(int a, int b) {
         result = a - b;
         return result;
     }
 
-    public static int mul(int a, int b) {
+    public int mul(int a, int b) {
         result = a * b;
         return result;
     }
 
-    public static int div(int a, int b) {
+    public int div(int a, int b) {
         result = a / b;
         return result;
     }
