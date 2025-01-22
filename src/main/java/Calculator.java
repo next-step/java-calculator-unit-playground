cf2d701583f78111f601558c202f6094c2fff701
JanooGwan <janoogwan39@gmail.com>
2025-01-22 16:14:08 +0900

    커밋 테스트

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public int plus(int x, int y) {
+        return x + y;
+    }
+
+    public int minus(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int devide(int x, int y) {
+        return x / y;
+    }
+}
