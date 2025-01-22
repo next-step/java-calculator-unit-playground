f171dd64f8550953c7dcacbe6204eb82e54c67ad
Donghun <2dh2@naver.com>
2025-01-23 02:51:19 +0900

    feat. 테스트 코드를 통과할 수 있도록 메소드 로직 수정

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,33 @@
 public class Calculator {
     public static int add(int num1, int num2) {
-        return num1 + num2;
+        try {
+            return Math.addExact(num1, num2);
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("연산 결과가 자료형 int의 범위를 벗어났습니다.");
+        }
     }
 
     public static int sub(int num1, int num2) {
-        return num1 - num2;
+        try {
+            return Math.subtractExact(num1, num2);
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("연산 결과가 자료형 int의 범위를 벗어났습니다.");
+        }
     }
 
     public static int mul(int num1, int num2) {
-        return num1 * num2;
+        try {
+            return Math.multiplyExact(num1, num2);
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("연산 결과가 자료형 int의 범위를 벗어났습니다.");
+        }
     }
 
     public static int div(int num1, int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+
         return num1 / num2;
     }
 }
