f17ff01d51205878b34de35f6fd20234ed7d1d1f
JanooGwan <janoogwan39@gmail.com>
2025-01-22 16:37:36 +0900

    예외 처리 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,21 @@
 public class Calculator {
     public int plus(int x, int y) {
+        if(x > Integer.MAX_VALUE - y) throw new IllegalArgumentException("두 수의 합이 계산 가능한 범위를 벗어났습니다.");
         return x + y;
     }
 
     public int minus(int x, int y) {
+        if(x < Integer.MIN_VALUE + y) throw new IllegalArgumentException("두 수의 차가 계산 가능한 범위를 벗어났습니다.");
         return x - y;
     }
 
     public int multiply(int x, int y) {
+        if(x > Integer.MAX_VALUE / y || x < Integer.MIN_VALUE / y) throw new IllegalArgumentException("두 수의 곱이 계산 가능한 범위를 벗어났습니다.");
         return x * y;
     }
 
     public int devide(int x, int y) {
+        if (y == 0) throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
         return x / y;
     }
 }
