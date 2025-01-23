45d822e71477355d9b0427d170e55f3944ecf09e
yeran27 <ranijini42@gmail.com>
2025-01-23 22:06:24 +0900

    메시지

---
+++ src/main/java/first.java
@@ -0,0 +1,17 @@
+public class first{
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+        return a / b;
+    }
+}
