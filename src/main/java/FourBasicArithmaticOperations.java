be9854a90eb8b2abeb674cc1239228e84c5ac929
illeum <kimsunga0322@gmail.com>
2025-01-23 14:56:03 +0900

    초간단 계산기 구현

---
+++ src/main/java/FourBasicArithmaticOperations.java
@@ -0,0 +1,17 @@
+public class FourBasicArithmaticOperations {
+    public int plus(int a, int b){
+        return a+b;
+    }
+
+    public int minus(int a, int b){
+        return a-b;
+    }
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+}
