a27763437a840abebf3071e5cf998dbae2360cff
Donghun <2dh2@naver.com>
2025-01-23 17:42:27 +0900

    feat. 계산기 중복되는 로직 모듈화

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,26 +1,24 @@
+import java.util.function.BiFunction;
+
 public class Calculator {
-    public static int add(int num1, int num2) {
+    public static int calculate(BiFunction<Integer, Integer, Integer> func, int num1, int num2) {
         try {
-            return Math.addExact(num1, num2);
+            return func.apply(num1, num2);
         } catch (ArithmeticException e) {
             throw new ArithmeticException("연산 결과가 자료형 int의 범위를 벗어났습니다.");
         }
+    }
+
+    public static int add(int num1, int num2) {
+        return calculate(Math::addExact, num1, num2);
     }
 
     public static int sub(int num1, int num2) {
-        try {
-            return Math.subtractExact(num1, num2);
-        } catch (ArithmeticException e) {
-            throw new ArithmeticException("연산 결과가 자료형 int의 범위를 벗어났습니다.");
-        }
+        return calculate(Math::subtractExact, num1, num2);
     }
 
     public static int mul(int num1, int num2) {
-        try {
-            return Math.multiplyExact(num1, num2);
-        } catch (ArithmeticException e) {
-            throw new ArithmeticException("연산 결과가 자료형 int의 범위를 벗어났습니다.");
-        }
+        return calculate(Math::multiplyExact, num1, num2);
     }
 
     public static int div(int num1, int num2) {
