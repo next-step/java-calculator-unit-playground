3d2a5b0dcaf140228d0541978eed634070ce8ec3
illeum <kimsunga0322@gmail.com>
2025-01-24 14:39:03 +0900

    계산기 예외처리, 테스트파일 작성

--- src/main/java/FourBasicArithmaticOperations.java
+++ src/main/java/FourBasicArithmaticOperations.java
@@ -1,17 +1,38 @@
+import javax.management.RuntimeErrorException;
+
 public class FourBasicArithmaticOperations {
     public int plus(int a, int b){
+        if((a>0 && b>0) && a>Integer.MAX_VALUE-b)   // 둘 다 양수
+            throw new RuntimeException("plus overflow");
+        if((a<0 && b<0) && a<Integer.MIN_VALUE-b)   // 둘 다 음수
+            throw new RuntimeException("plus overflow");
+
         return a+b;
     }
 
     public int minus(int a, int b){
+        if((a>0 && b<0) && a>Integer.MAX_VALUE+b)
+            throw new RuntimeException("minus overflow");
+        if((a<0 && b>0) && a<Integer.MIN_VALUE+b)
+            throw new RuntimeException("minus overflow");
+
         return a-b;
     }
 
     public int multiply(int a, int b){
+        if(a==0 || b==0)    return 0;   // overflow 체크 대비
+        if(a>Integer.MAX_VALUE/b || a<Integer.MIN_VALUE/b)
+            throw new RuntimeException("multiply overflow");
+
         return a*b;
     }
 
-    public int divide(int a, int b){
-        return a/b;
+    public int divide(int a, int div){
+        if(div==0)
+            throw new RuntimeException("div by 0");
+        if(a==Integer.MIN_VALUE && div==-1)
+            throw new RuntimeException("divide overflow");
+
+        return a/div;
     }
 }
---
+++ src/test/java/FourBasicArithmaticOperationsTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("사칙연산 계산기 테스트")
+public class FourBasicArithmaticOperationsTest {
+
+    @Test
+    public void plusTest(){
+        FourBasicArithmaticOperations oper = new FourBasicArithmaticOperations();
+        assertEquals(3, oper.plus(1, 2));
+        assertEquals(1, oper.plus(-1, 2));
+        assertEquals(-1, oper.plus(1, -2));
+        assertEquals(-3, oper.plus(-1, -2));
+        assertThrows(RuntimeException.class, ()->oper.plus(Integer.MAX_VALUE, 1));
+        assertThrows(RuntimeException.class, ()->oper.plus(Integer.MIN_VALUE, -1));
+        assertEquals(0, oper.plus(0, 0));
+    }
+
+    @Test
+    public void minusTest(){
+        FourBasicArithmaticOperations oper = new FourBasicArithmaticOperations();
+        assertEquals(-1, oper.minus(1, 2));
+        assertEquals(-3, oper.minus(-1, 2));
+        assertEquals(3, oper.minus(1, -2));
+        assertEquals(1, oper.minus(-1, -2));
+        assertEquals(Integer.MIN_VALUE+1, oper.minus(0, Integer.MAX_VALUE));
+        assertThrows(RuntimeException.class, ()->oper.minus(-10, Integer.MAX_VALUE));
+        assertThrows(RuntimeException.class, ()->oper.minus(Integer.MIN_VALUE, 1));
+        assertEquals(0, oper.minus(0, 0));
+    }
+
+    @Test
+    public void multiplyTest(){
+        FourBasicArithmaticOperations oper = new FourBasicArithmaticOperations();
+        assertEquals(6, oper.multiply(2, 3));
+        assertEquals(0, oper.multiply(2, 0));
+        assertEquals(0, oper.multiply(0, 3));
+        assertEquals(0, oper.multiply(0, 0));
+        assertThrows(RuntimeException.class, ()->oper.multiply(1000000, 1000000));
+    }
+
+    @Test
+    public void divideTest(){
+        FourBasicArithmaticOperations oper = new FourBasicArithmaticOperations();
+        assertEquals(5, oper.divide(10, 2));
+        assertEquals(1, oper.divide(3, 2));
+        assertEquals(-1, oper.divide(-3, 2));
+        assertThrows(RuntimeException.class, ()->oper.divide(3, 0));
+        assertEquals(0, oper.divide(0, 1));
+    }
+
+}
