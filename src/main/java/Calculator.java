3ca2bde6a784bf57ebaf3f022dd8ff6b03099337
JanooGwan <janoogwan39@gmail.com>
2025-01-25 22:21:34 +0900

    테스트 프로그램 작성 및 Calculator.java 수정

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,12 +10,13 @@ public class Calculator {
     }
 
     public int multiply(double x, double y) {
-        if(x > Integer.MAX_VALUE / y || x < Integer.MIN_VALUE / y) throw new IllegalArgumentException("두 수의 곱이 계산 가능한 범위를 벗어났습니다.");
+        if(x * y > Integer.MAX_VALUE || x / y < Integer.MIN_VALUE) throw new IllegalArgumentException("두 수의 곱이 계산 가능한 범위를 벗어났습니다.");
         return (int)(x * y);
     }
 
     public int devide(double x, double y) {
         if (y == 0) throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+        if(x / y > Integer.MAX_VALUE || x / y < Integer.MIN_VALUE) throw new IllegalArgumentException("두 수의 나눗셈이 계산 가능한 범위를 벗어났습니다.");
         return (int)(x / y);
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,57 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Nested
+    class CalculatorMethodTest {
+        @Test
+        @DisplayName("덧셈 테스트")
+        void testAdd() {
+            assertEquals(3, calculator.plus(1, 2));
+            assertEquals(-5, calculator.plus(30, -35));
+            assertEquals(4, calculator.plus(3.6, 0.4));
+            assertEquals(5, calculator.plus(4.1, 1));
+            assertThrows(IllegalArgumentException.class, () -> calculator.plus(1, Integer.MAX_VALUE));
+        }
+
+        @Test
+        @DisplayName("뺄셈 테스트")
+        void testMinus() {
+            assertEquals(5, calculator.minus(10, 5));
+            assertEquals(-10, calculator.minus(10, 20));
+            assertEquals(3, calculator.minus(7.1, 4.1));
+            assertEquals(4, calculator.minus(-1, -5.9));
+            assertThrows(IllegalArgumentException.class, () -> calculator.minus(Integer.MIN_VALUE, 1));
+        }
+
+        @Test
+        @DisplayName("곱셈 테스트")
+        void testMultiply() {
+            assertEquals(10, calculator.multiply(2, 5));
+            assertEquals(0, calculator.multiply(1, 0));
+            assertEquals(8, calculator.multiply(3.5, 2.4));
+            assertEquals(-4, calculator.multiply(2, -2));
+            assertEquals(10, calculator. multiply(-2.5, -4));
+            assertThrows(IllegalArgumentException.class, () -> calculator.multiply(Integer.MAX_VALUE, 2));
+            assertThrows(IllegalArgumentException.class, () -> calculator.multiply(Integer.MIN_VALUE, 0.5));
+        }
+
+        @Test
+        @DisplayName("나눗셈 테스트")
+        void testDevide() {
+            assertThrows(IllegalArgumentException.class, () -> calculator.devide(10, 0));
+            assertEquals(2, calculator.devide(10, 5));
+            assertEquals(0, calculator.devide(0, 10));
+            assertEquals(-3, calculator.devide(12, -4));
+            assertEquals(6, calculator.devide(-30, -5));
+            assertEquals(4, calculator.devide(8.6, 2));
+            assertThrows(IllegalArgumentException.class, () -> calculator.devide(Integer.MIN_VALUE, 0.5));
+            assertThrows(IllegalArgumentException.class, () -> calculator.devide(Integer.MAX_VALUE, 0.25));
+        }
+    }
 }
