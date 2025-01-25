2514410024c9f4a9aa2fb549166c6c4c05cde780
digital8150 <wptpgud@gmail.com>
2025-01-26 01:19:17 +0900

    sc -> stringCalculator

--- src/test/java/TestStringCalculator.java
+++ src/test/java/TestStringCalculator.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("문자열 계산기 단위 테스트")
 public class TestStringCalculator {
 
-    StringCalculator sc = new StringCalculator();
+    StringCalculator stringCalculator = new StringCalculator();
 
     @Nested
     @DisplayName("문자열 계산기 기능 테스트")
@@ -17,51 +17,51 @@ public class TestStringCalculator {
         @Test
         @DisplayName("빈 문자열 케이스")
         void testEmptyString(){
-            assertEquals(0, sc.addAll(""));
+            assertEquals(0, stringCalculator.addAll(""));
         }
 
         @Test
         @DisplayName("올바른 입력 케이스")
         void testValidInputs(){
             //기본 구분자 사용
-            assertEquals(10,sc.addAll("1:2:3:4"));
-            assertEquals(10,sc.addAll("1,2,3,4"));
-            assertEquals(6,sc.addAll("1,2,3"));
-            assertEquals(6,sc.addAll("1:2:3"));
-            assertEquals(3,sc.addAll("1,2"));
-            assertEquals(3,sc.addAll("1:2"));
-            assertEquals(1,sc.addAll("1"));
+            assertEquals(10,stringCalculator.addAll("1:2:3:4"));
+            assertEquals(10,stringCalculator.addAll("1,2,3,4"));
+            assertEquals(6,stringCalculator.addAll("1,2,3"));
+            assertEquals(6,stringCalculator.addAll("1:2:3"));
+            assertEquals(3,stringCalculator.addAll("1,2"));
+            assertEquals(3,stringCalculator.addAll("1:2"));
+            assertEquals(1,stringCalculator.addAll("1"));
 
             //커스텀 구분자 사용
-            assertEquals(10,sc.addAll("//;\n1;2;3;4"));
-            assertEquals(6,sc.addAll("//;\n1;2;3"));
-            assertEquals(3,sc.addAll("//;\n1;2"));
-            assertEquals(1,sc.addAll("//;\n1"));
+            assertEquals(10,stringCalculator.addAll("//;\n1;2;3;4"));
+            assertEquals(6,stringCalculator.addAll("//;\n1;2;3"));
+            assertEquals(3,stringCalculator.addAll("//;\n1;2"));
+            assertEquals(1,stringCalculator.addAll("//;\n1"));
 
-            assertEquals(10,sc.addAll("//^\n1^2^3^4"));
-            assertEquals(6,sc.addAll("//^\n1^2^3"));
-            assertEquals(3,sc.addAll("//^\n1^2"));
-            assertEquals(1,sc.addAll("//^\n1"));
+            assertEquals(10,stringCalculator.addAll("//^\n1^2^3^4"));
+            assertEquals(6,stringCalculator.addAll("//^\n1^2^3"));
+            assertEquals(3,stringCalculator.addAll("//^\n1^2"));
+            assertEquals(1,stringCalculator.addAll("//^\n1"));
         }
 
         @Test
         @DisplayName("RuntimeException 발생 케이스")
         void testRuntimeException() {
             //숫자 이외의 값
-            assertThrows(RuntimeException.class, () -> sc.addAll("1,a,3"));
-            assertThrows(RuntimeException.class, () -> sc.addAll("1:a:3"));
-            assertThrows(RuntimeException.class, () -> sc.addAll("//^\n1^a^3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("1,a,3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("1:a:3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("//^\n1^a^3"));
 
             //음수 입력 케이스
-            assertThrows(RuntimeException.class, () -> sc.addAll("1,-2,3"));
-            assertThrows(RuntimeException.class, () -> sc.addAll("1:-2:3"));
-            assertThrows(RuntimeException.class, () -> sc.addAll("//^\n1^-2^3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("1,-2,3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("1:-2:3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("//^\n1^-2^3"));
 
             //잘못된 커스텀 지정자 구문
-            assertThrows(RuntimeException.class, () -> sc.addAll("//;\t1;2;3"));
-            assertThrows(RuntimeException.class, () -> sc.addAll("//;1;2;3"));
-            assertThrows(RuntimeException.class, () -> sc.addAll("1^2^3"));
-            assertThrows(RuntimeException.class, () -> sc.addAll("^\n1^2^3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("//;\t1;2;3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("//;1;2;3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("1^2^3"));
+            assertThrows(RuntimeException.class, () -> stringCalculator.addAll("^\n1^2^3"));
 
         }
     }
