9433de93a20fc9e6be176bb29f967c07aa850e3c
digital8150 <wptpgud@gmail.com>
2025-01-25 13:21:07 +0900

    매개변수 이름 수정

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,22 +3,22 @@ public class Calculator {
     //인자 2개를 받아 정수 결과를 반환
     //사칙연산을 위한 메서드 4개르 제공
 
-    public int add(int a, int b){
-        return a+b;
+    public int add(int left, int right){
+        return left+right;
     }
 
-    public int sub(int a, int b){
-        return a-b;
+    public int sub(int left, int right){
+        return left-right;
     }
 
-    public int mul(int a, int b){
-        return a*b;
+    public int mul(int left, int right){
+        return left*right;
     }
 
-    public int div(int a, int b){
-        if(b == 0){
+    public int div(int left, int right){
+        if(right == 0){ //우변이 0인 경우 -> 0으로 나누는 문제
             throw new RuntimeException("0으로는 나눌 수 없습니다");
         }
-        return a/b;
+        return left/right;
     }
 }
