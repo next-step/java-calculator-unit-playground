b42f5ea1853c1091f240b9875f484981e8aade25
minjun-oh <a39229121@gmail.com>
2025-01-26 00:03:39 +0900

    step1

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,9 +2,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class Calculator_Test {
+public class CalculatorTest {
     @Nested
     @DisplayName("Simple Calculator")
     class Calculator_Simple_Test {
@@ -51,9 +51,50 @@ public class Calculator_Test {
         }
 
         @Test
-        void divTest2(){
+        void divTest2() {
             Calculator_Simple cal = new Calculator_Simple();
             assertEquals(21, cal.div(3234, 153));
+        }
+    }
+    @Nested
+    @DisplayName("String Calculator")
+    class String_Calculator_Test {
+
+        @Test
+        void testEmpty(){
+            assertEquals(0, Calculator.sum(""));
+        }
+
+        @Test
+        void testOne(){
+            assertEquals(1, Calculator.sum("1"));
+        }
+
+        @Test
+        void testTwo(){
+            assertEquals(3, Calculator.sum("1,2"));
+        }
+
+        @Test
+        void testThree(){
+            assertEquals(6, Calculator.sum("1,2,3"));
+        }
+
+        @Test
+        void testCustom() {
+            assertEquals(6, Calculator.sum("//;\n1;2;3"));
+        }
+
+        @Test
+        void testNegativeNumber(){
+            RuntimeException thrown = assertThrows(RuntimeException.class, () -> Calculator.sum("1,-2,3"));
+            assertTrue(thrown.getMessage().contains("음수가 입력되었습니다"));
+        }
+
+        @Test
+        void testInvalidNumber(){
+            RuntimeException thrown = assertThrows(RuntimeException.class, () -> Calculator.sum("1,a,3"));
+            assertTrue(thrown.getMessage().contains("유효하지 않은"));
         }
     }
 }
