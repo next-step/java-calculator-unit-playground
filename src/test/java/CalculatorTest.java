e7099ad9e96b46fdaa76d18704dc75bbc8770286
JanooGwan <janoogwan39@gmail.com>
2025-01-25 23:48:19 +0900

    테스트 프로그램 AssertJ 기반으로 리팩토링 완료

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
 
 public class CalculatorTest {
     Calculator calculator = new Calculator();
@@ -12,46 +12,53 @@ public class CalculatorTest {
         @Test
         @DisplayName("덧셈 테스트")
         void testAdd() {
-            assertEquals(3, calculator.plus(1, 2));
-            assertEquals(-5, calculator.plus(30, -35));
-            assertEquals(4, calculator.plus(3.6, 0.4));
-            assertEquals(5, calculator.plus(4.1, 1));
-            assertThrows(IllegalArgumentException.class, () -> calculator.plus(1, Integer.MAX_VALUE));
+            Assertions.assertThat(calculator.plus(1, 2)).isEqualTo(3);
+            Assertions.assertThat(calculator.plus(30, -35)).isEqualTo(-5);
+            Assertions.assertThat(calculator.plus(3.6, 0.4)).isEqualTo(4);
+            Assertions.assertThat(calculator.plus(4.1, 1)).isEqualTo(5);
+            Assertions.assertThatThrownBy(() -> calculator.plus(1, Integer.MAX_VALUE))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         @DisplayName("뺄셈 테스트")
         void testMinus() {
-            assertEquals(5, calculator.minus(10, 5));
-            assertEquals(-10, calculator.minus(10, 20));
-            assertEquals(3, calculator.minus(7.1, 4.1));
-            assertEquals(4, calculator.minus(-1, -5.9));
-            assertThrows(IllegalArgumentException.class, () -> calculator.minus(Integer.MIN_VALUE, 1));
+            Assertions.assertThat(calculator.minus(10, 5)).isEqualTo(5);
+            Assertions.assertThat(calculator.minus(10, 20)).isEqualTo(-10);
+            Assertions.assertThat(calculator.minus(7.1, 4.1)).isEqualTo(3);
+            Assertions.assertThat(calculator.minus(-1, -5.9)).isEqualTo(4);
+            Assertions.assertThatThrownBy(() -> calculator.minus(Integer.MIN_VALUE, 1))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         @DisplayName("곱셈 테스트")
         void testMultiply() {
-            assertEquals(10, calculator.multiply(2, 5));
-            assertEquals(0, calculator.multiply(1, 0));
-            assertEquals(8, calculator.multiply(3.5, 2.4));
-            assertEquals(-4, calculator.multiply(2, -2));
-            assertEquals(10, calculator. multiply(-2.5, -4));
-            assertThrows(IllegalArgumentException.class, () -> calculator.multiply(Integer.MAX_VALUE, 2));
-            assertThrows(IllegalArgumentException.class, () -> calculator.multiply(Integer.MIN_VALUE, 0.5));
+            Assertions.assertThat(calculator.multiply(2, 5)).isEqualTo(10);
+            Assertions.assertThat(calculator.multiply(1, 0)).isEqualTo(0);
+            Assertions.assertThat(calculator.multiply(3.5, 2.4)).isEqualTo(8);
+            Assertions.assertThat(calculator.multiply(2, -2)).isEqualTo(-4);
+            Assertions.assertThat(calculator.multiply(-2.5, -4)).isEqualTo(10);
+            Assertions.assertThatThrownBy(() -> calculator.multiply(Integer.MAX_VALUE, 2))
+                    .isInstanceOf(IllegalArgumentException.class);
+            Assertions.assertThatThrownBy(() -> calculator.multiply(Integer.MIN_VALUE, 0.5))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         @DisplayName("나눗셈 테스트")
         void testDevide() {
-            assertThrows(IllegalArgumentException.class, () -> calculator.devide(10, 0));
-            assertEquals(2, calculator.devide(10, 5));
-            assertEquals(0, calculator.devide(0, 10));
-            assertEquals(-3, calculator.devide(12, -4));
-            assertEquals(6, calculator.devide(-30, -5));
-            assertEquals(4, calculator.devide(8.6, 2));
-            assertThrows(IllegalArgumentException.class, () -> calculator.devide(Integer.MIN_VALUE, 0.5));
-            assertThrows(IllegalArgumentException.class, () -> calculator.devide(Integer.MAX_VALUE, 0.25));
+            Assertions.assertThatThrownBy(() -> calculator.devide(10, 0))
+                    .isInstanceOf(IllegalArgumentException.class);
+            Assertions.assertThat(calculator.devide(10, 5)).isEqualTo(2);
+            Assertions.assertThat(calculator.devide(0, 10)).isEqualTo(0);
+            Assertions.assertThat(calculator.devide(12, -4)).isEqualTo(-3);
+            Assertions.assertThat(calculator.devide(-30, -5)).isEqualTo(6);
+            Assertions.assertThat(calculator.devide(8.6, 2)).isEqualTo(4);
+            Assertions.assertThatThrownBy(() -> calculator.devide(Integer.MIN_VALUE, 0.5))
+                    .isInstanceOf(IllegalArgumentException.class);
+            Assertions.assertThatThrownBy(() -> calculator.devide(Integer.MAX_VALUE, 0.25))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
