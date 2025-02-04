86f186fa24c4f4ad24eb9735c68e75f246530945
TwooTwoo <mbnman12@gmail.com>
2025-02-04 22:04:31 +0900

    fix : assertEquals 및 assertThrows 메서드 실패시 출력되는 안내 메세지 수정

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+
+        return a / b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    private final Calculator cal = new Calculator();
+
+    @Nested
+    @DisplayName("덧셈 테스트 클래스")
+    class AddTest {
+        @Test
+        @DisplayName("덧셈 테스트 메서드")
+        void add() {
+            assertEquals(10, cal.add(3, 7), "덧셈 테스트 실패");
+        }
+    }
+
+    @Nested
+    @DisplayName("뺄셈 테스트 클래스")
+    class SubtractTest {
+        @Test
+        @DisplayName("뺄셈 테스트 메서드")
+        void subtract() {
+            assertEquals(4, cal.subtract(7, 3), "뺄셈 테스트 실패");
+        }
+    }
+
+    @Nested
+    @DisplayName("곱셈 테스트 클래스")
+    class MultiplyTest {
+        @Test
+        @DisplayName("곱셈 테스트 메서드")
+        void multiply() {
+            assertEquals(10, cal.multiply(2, 5), "곱셈 테스트 실패");
+        }
+    }
+
+    @Nested
+    @DisplayName("나눗셈 테스트 클래스")
+    class DivideTest {
+        @Test
+        @DisplayName("나눗셈 테스트 메서드, 0으로 나누는 경우")
+        void divideWithZero() {
+            assertThrows(ArithmeticException.class, () -> cal.divide(10, 0), "0으로 나누어 테스트 실패");
+        }
+
+        @Test
+        @DisplayName("나눗셈 테스트 메서드")
+        void divide() {
+            assertEquals(2, cal.divide(10, 5), "나눗셈 테스트 실패");
+        }
+    }
+
 }
