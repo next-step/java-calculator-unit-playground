5ea3434fe8704bc6ddfeb7adf4f61c5a84cee4cd
윤준석(YunJunSeok) <wicaduce@gmail.com>
2025-02-04 22:04:20 +0900

    Fix: assertEquals, assertThrows의 세 번째 인자는 테스트 실패 시 출력할 메시지이나, 성공으로 작성된 문구를 수정하였습니다.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
         @Test
         @DisplayName("더하기_테스트_case1:1+2")
         void add(){
-            assertEquals(3, calc.add(1,2),"더하기(+)_테스트_통과!");
+            assertEquals(3, calc.add(1,2),"더하기(+)_테스트_실패.");
         }
     }
 
@@ -24,7 +24,7 @@ public class CalculatorTest {
         @Test
         @DisplayName("빼기_테스트_case1:2-1")
         void subtract(){
-            assertEquals(1, calc.subtract(2,1),"빼기(-)_테스트_통과!");
+            assertEquals(1, calc.subtract(2,1),"빼기(-)_테스트_실패.");
         }
     }
 
@@ -34,7 +34,7 @@ public class CalculatorTest {
         @Test
         @DisplayName("곱하기_테스트_case1:3*4")
         void multiply(){
-            assertEquals(12, calc.multiply(3,4),"곱하기(*)_테스트_통과!");
+            assertEquals(12, calc.multiply(3,4),"곱하기(*)_테스트_실패.");
         }
     }
 
@@ -44,13 +44,13 @@ public class CalculatorTest {
         @Test
         @DisplayName("나누기_테스트_case1:2/1")
         void divide(){
-            assertEquals(2, calc.divide(2,1),"나누기(/)_테스트_통과!");
+            assertEquals(2, calc.divide(2,1),"나누기(/)_테스트_실패.");
         }
 
         @Test
         @DisplayName("나누기_테스트_case2:0을_이용한_나눗셈")
         void divideWithZero(){
-            assertThrows(ArithmeticException.class, () -> calc.divide(2,0),"나누기(/)_테스트(0을_이용한_나눗셈)통과!");
+            assertThrows(ArithmeticException.class, () -> calc.divide(2,0),"나누기(/)_테스트(0을_이용한_나눗셈)실패.");
         }
     }
 }
