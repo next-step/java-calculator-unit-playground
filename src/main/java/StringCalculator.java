607a21a8ae4cb1c99fffc92743c07ff744c4c2e5
ralph <sanghwaj222@gmail.com>
2025-02-08 17:31:07 +0900

    [feat/#2] refactor main logic and add customDelimiterAdd test

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,28 +1,33 @@
+import java.util.Arrays;
+
 public class StringCalculator {
-    public Long StringToSumOfLong(String str) {
+    public Integer StringToSumOfInt(String str) {
         if (str == null || str.isEmpty()) {
-            return 0L;
+            return 0;
         }
 
         String delimiter = "[,|:]";
+        String[] words = null;
 
         if (str.startsWith("//")) {
             int customDelimiterIndex = str.indexOf("\n");
             if (customDelimiterIndex != -1) {
                 // "//"는 인덱스 0,1을 차지할 테니, 2부터 "\n"인덱스 까지의 문자열이 delimiter가 됨.
                 delimiter = str.substring(2, customDelimiterIndex);
+                // "\n"문자열을 기준으로 앞의 문자열을 날리고 설정한 delimiter로 split
+                words = str.substring(customDelimiterIndex + 1).split(delimiter);
             }
+        } else {
+            words = str.split(delimiter);
         }
 
-        Long sum = 0L;
-        String[] words = str.split(delimiter);
+        int sum = 0;
 
-        for(int i=0; i<words.length; i++) {
-            if (Long.parseLong(words[i]) < 0) {
+        for (int i = 0; i < words.length; i++) {
+            if (Integer.parseInt(words[i]) < 0) {
                 throw new RuntimeException("negative number is not allowed");
             }
-            sum += Long.parseLong(words[i]);
-            i++;
+            sum += Integer.parseInt(words[i]);
         }
         return sum;
     }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,13 +1,23 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    private final StringCalculator stringCalculator = new StringCalculator();
 
     @Test
-    @DisplayName("basic Test")
-    void stringToSumOfLong() {
+    void basicAdd() {
+        assertThat(stringCalculator.StringToSumOfInt("1,2")).as("The result 1 + 2 must be 3").isEqualTo(3);
+    }
 
+    @Test
+    void customDelimiterAdd() {
+        assertThat(stringCalculator.StringToSumOfInt("//q\n1q2")).as("The result 1 + 2 must be 3").isEqualTo(3);
     }
 }
