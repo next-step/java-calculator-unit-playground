0afd62f783f374ec2eedf5ff6969561df0460fdf
haeunsong <jamy022576@gmail.com>
2025-02-08 17:37:28 +0900

    feat: 미션 1,2단계 + 미션 3,4단계

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//한글 메서드명 대신 @DisplayName으로 쓰자
+@DisplayName("초간단 계산기 테스트")
+class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    //@Nested 중첩 클래스 선언, 여러가지 메서드를 사용할 수 있음.
+    @Nested
+    class Add {
+        @Test
+        void add() {
+            assertEquals(5, calculator.add(2, 3));
+        }
+    }
+
+    @Nested
+    class Subtract {
+        @Test
+        void subtract() {
+            assertEquals(-3, calculator.subtract(3, 6));
+        }
+    }
+
+    @Nested
+    class Multiply {
+        @Test
+        void multiply() {
+            assertEquals(6, calculator.multiply(2, 3));
+        }
+    }
+
+    @Nested
+    class Divide{
+        @Test
+        @DisplayName("0으로 나누면 ArithmeticException 예외가 발생한다.")
+        void should_throw_exception_when_divide_by_zero() {
+            assertThrows(ArithmeticException.class, () -> calculator.divide(3,0));
+        }
+
+        @Test
+        void divideByNotZero() {
+            assertEquals(2, calculator.divide(6, 3));
+        }
+    }
+}
