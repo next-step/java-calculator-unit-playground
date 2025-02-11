15dad5413bfbaaa741e812689805a00b6d173cd4
haeunsong <jamy022576@gmail.com>
2025-02-12 02:24:42 +0900

    refactor : 코드 리뷰 반영하여 수정
    - should_throw_exception_when_another_wrong_input 이 경우 테스트를 통과하지 못했습니다. 차근차근 다시 해보겠습니다.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,52 +2,90 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public int getSum(String str) {
+    /*
+        1. 입력값 검증
+        2. 구분자 추출
+        3. 문자열을 구분자 기준으로 분리
+        4. 분리된 숫자 문자열 배열을 합산
+     */
 
+    public int splitStrBySeparaterAndGetSum(String str) {
+        // 1. 입력값 검증
         if(str.length() == 0) {
             return 0;
         }
-        int sum = 0;
+
+        // 2. 구분자, 커스텀 구분자일 경우 구분자 제외 검사해야할 문자열 추출
+        String separater = extractSeparater(str);
+        String numbers = extractNumbers(str);
+
+        // 3. 문자열을 구분자 기준으로 분리
+        String [] tokens = numbers.split(separater);
+        validateTokens(tokens);
+
+        // 4. 분리된 숫자 문자열 배열 합산
+        return calculateSum(tokens);
+    }
+
+    private String extractSeparater(String str) {
+        // 기본 구분자는 , 또는 : 이다.
         String separater = "[,|:]";
-        String numbers = str;
 
+        // 커스텀 구분자가 존재하는 경우
         if (str.startsWith("//") && str.contains("\n")) {
-
             int newLineIdx = str.indexOf("\n");
-            // /n 을 찾을 수 없거나, \n 이후 값이 없는 경우
+            // /n 을 찾을 수 없는 경우
             if (newLineIdx == -1) {
-                throw new RuntimeException("잘못된 입력 형식입니다.");
+                throw new RuntimeException("커스텀 구분자를 '//' 와 '\\n' 문자 사이에 정확하게 입력해주세요.");
             }
             separater = Pattern.quote(str.substring(2, newLineIdx));
-            numbers = str.substring(newLineIdx + 1); // \n 다음부터 끝까지 숫자가 있는 부분들
+        }
+        return separater;
+    }
 
-            // \n 이후에 최소한 구분자가 한 개 이상은 있어야한다. 구분자가 없다면 배열 길이가 1이된다.
-            if(numbers.isEmpty() || numbers.split(separater).length == 1) {
+    private String extractNumbers(String str) {
+        String numbers = "";
+
+        // 커스텀 구분자일 경우
+        if(str.startsWith("//")) {
+            numbers = str.substring(str.indexOf("\n") + 1);
+            return numbers;
+        }
+
+        // 기본 구분자이면 그대로 str 반환
+        return str;
+    }
+
+    private void validateTokens(String[] tokens) {
+        for(String token : tokens) {
+            if (token.isEmpty()) {
                 throw new RuntimeException("잘못된 입력 형식입니다.");
             }
-        }
-        // 구분자를 기준으로 split 진행 후 sum 실행
-        String[] tokens = numbers.split(separater);
-        if(tokens.length == 1) {
-            return 0;
-        }
-        for (String token : tokens) {
-            int num = parseNumber(token);
-            if (num < 0) {
+
+            if(parseNumber(token) < 0) {
                 throw new RuntimeException("음수는 사용할 수 없습니다.");
             }
-            sum += num;
         }
+    }
 
+    private int calculateSum(String[] tokens) {
+        int sum = 0;
+        for(String token : tokens) {
+            sum += Integer.parseInt(token);
+        }
         return sum;
     }
 
-    public int parseNumber(String number) {
+    private int parseNumber(String number) {
         // 숫자가 아니라면 예외 발생
         try {
             return Integer.parseInt(number);
         } catch (NumberFormatException e) {
             throw new RuntimeException("숫자가 아닌 값이 포함되어 있습니다.");
         }
+    }
+
+    private boolean isNumeric(String str) {
+        return str.matches("-?\\d+");  // 정수 여부를 확인하는 정규식 (-는 음수 허용)
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -8,25 +10,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("문자열 계산기 테스트")
 class StringCalculatorTest {
 
-    StringCalculator stringCalculator = new StringCalculator();
+    private StringCalculator stringCalculator = new StringCalculator();
 
     @Nested
     @DisplayName("기본 구분자 테스트")
     class basicSeparaterTest {
-        @Test
-        @DisplayName("빈 문자열이면 0을 반환해야 한다.")
-        void should_return_0_when_string_is_empty() {
-            assertThat(0).isEqualTo(stringCalculator.getSum(""));
-        }
-        @Test
-        @DisplayName("구분자가 없는 경우 0을 반환해야 한다.")
-        void test() {
-            assertThat(0).isEqualTo(stringCalculator.getSum("123"));
-        }
-        @Test
-        @DisplayName("쉼표(,) 또는 콜론(:)으로 구분된 숫자들의 합을 반환해야 한다.")
-        void should_sum_numbers_separated_by_comma_or_colon() {
-            assertThat(6).isEqualTo(stringCalculator.getSum("1,2:3"));
+
+        @ParameterizedTest(name = "입력값 \"{0}\"은 결과값 {1}을 반환해야 한다.")
+        @CsvSource({
+                "'', 0",
+                "'123', 123",
+                "'1,2:3', 6"
+        })
+        void should_return_correct_sum(String input, int expected) {
+            assertThat(stringCalculator.splitStrBySeparaterAndGetSum(input)).isEqualTo(expected);
         }
     }
 
@@ -36,7 +33,8 @@ class StringCalculatorTest {
         @Test
         @DisplayName("커스텀 구분자로 분리한 숫자들의 합을 반환해야 한다")
         void should_sum_numbers_separated_by_custom_separator() {
-            assertThat(6).isEqualTo(stringCalculator.getSum("//;\n1;2;3"));
+            assertThat(6).isEqualTo(stringCalculator.splitStrBySeparaterAndGetSum("//;\n1;2;3"));
+            assertThat(2).isEqualTo(stringCalculator.splitStrBySeparaterAndGetSum("//;\n2"));
         }
     }
 
@@ -48,16 +46,16 @@ class StringCalculatorTest {
         void should_throw_exception_when_contains_non_number() {
 
             // 특수문자가 포함된 경우
-            assertThatThrownBy(() -> stringCalculator.getSum("//;\n1;*;3"))
+            assertThatThrownBy(() -> stringCalculator.splitStrBySeparaterAndGetSum("//;\n1;*;3"))
                     .isInstanceOf(RuntimeException.class)
                     .hasMessageContaining("숫자가 아닌 값이 포함되어 있습니다.");
 
-            assertThatThrownBy(() -> stringCalculator.getSum("//;\n1456;-;3"))
+            assertThatThrownBy(() -> stringCalculator.splitStrBySeparaterAndGetSum("//;\n1456;-;3"))
                     .isInstanceOf(RuntimeException.class)
                     .hasMessageContaining("숫자가 아닌 값이 포함되어 있습니다.");
 
             // 글자가 포함된 경우
-            assertThatThrownBy(() -> stringCalculator.getSum("//;\nddd;ad;3"))
+            assertThatThrownBy(() -> stringCalculator.splitStrBySeparaterAndGetSum("//;\nddd;ad;3"))
                     .isInstanceOf(RuntimeException.class)
                     .hasMessageContaining("숫자가 아닌 값이 포함되어 있습니다.");
         }
@@ -66,7 +64,7 @@ class StringCalculatorTest {
         @DisplayName("음수가 포함되면 예외를 발생해야 한다.")
         void should_throw_exception_when_contains_minus() {
 
-            assertThatThrownBy(() -> stringCalculator.getSum("-4,2:-9"))
+            assertThatThrownBy(() -> stringCalculator.splitStrBySeparaterAndGetSum("-4,2:-9"))
                     .isInstanceOf(RuntimeException.class)
                     .hasMessageContaining("음수는 사용할 수 없습니다.");
         }
@@ -75,11 +73,7 @@ class StringCalculatorTest {
         @DisplayName("또 다른 잘못된 입력 형식의 경우 예외를 발생해야 한다.")
         void should_throw_exception_when_another_wrong_input() {
 
-            assertThatThrownBy(() -> stringCalculator.getSum("//;1;2;\n;"))
-                    .isInstanceOf(RuntimeException.class)
-                    .hasMessageContaining("잘못된 입력 형식입니다.");
-
-            assertThatThrownBy(() -> stringCalculator.getSum("//;1;2;\n123"))
+            assertThatThrownBy(() -> stringCalculator.splitStrBySeparaterAndGetSum("//;1;2;\n;"))
                     .isInstanceOf(RuntimeException.class)
                     .hasMessageContaining("잘못된 입력 형식입니다.");
 
