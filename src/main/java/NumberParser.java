95b22bf332266bb71319c83e203a883c6bacfec5
권장순 <jsoonworld@gmail.com>
2025-04-29 00:22:28 +0900

    feat(NumberParser) : 문자열 입력을 검증하고 Number 객체로 변환하는 기능 구현

---
+++ src/main/java/NumberParser.java
@@ -0,0 +1,13 @@
+public class NumberParser {
+
+    private static final String ERROR_NOT_A_NUMBER = "[ERROR] 입력은 숫자여야 합니다.";
+
+    public static Number parse(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            return new Number(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_NOT_A_NUMBER);
+        }
+    }
+}
