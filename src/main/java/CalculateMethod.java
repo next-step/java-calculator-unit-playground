21f0f024d8346d482f32cc4037f4e1578489c779
wosyh18 <wosyh18@naver.com>
2025-04-28 14:26:03 +0900

    feat : 사칙연산 구현

---
+++ src/main/java/CalculateMethod.java
@@ -0,0 +1,11 @@
+public class CalculateMethod {
+    public int[] calculate(int a, int b) {
+        int plus = a + b;
+        int min = (a > b) ? (a - b) : (b - a);
+        int mul = a * b;
+        int div = a / b;
+
+        int[] result = {plus, min, mul, div};
+        return result;
+    }
+}
