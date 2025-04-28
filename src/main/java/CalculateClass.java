3556cbd142c8c49a19238fabdd220cfbf12c5a95
wosyh18 <80562216+wosyh18@users.noreply.github.com>
2025-04-28 20:48:47 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/CalculateClass.java
@@ -0,0 +1,11 @@
+public class CalculateClass {
+    public int add(int a, int b) {return a + b;}
+    public int min(int a, int b) {
+        return a > b ? ( a - b ) : (b - a );
+    }
+    public int div(int a, int b) {return a / b;}
+    public int mul (int a, int b) {return a * b;}
+}
+
+
+
