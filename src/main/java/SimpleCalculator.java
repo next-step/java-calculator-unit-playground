5cecb388623cf64c4de17d030245b66588f413c2
kimYejin <kimphoby0125@gmail.com>
2025-04-28 21:52:55 +0900

    style : 코드 포멧팅

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,20 +1,20 @@
 public class SimpleCalculator {
 
-    public static int add(int x, int y){
+    public static int add(int x, int y) {
         return x + y;
     }
 
-    public static int substract(int x, int y){
+    public static int substract(int x, int y) {
         return x - y;
     }
 
-    public static int multiply(int x, int y){
+    public static int multiply(int x, int y) {
         return x * y;
     }
 
-    public static int divide (int x, int y){
+    public static int divide(int x, int y) {
         if (y == 0) throw new RuntimeException("0으로 나눌 수 없습니다.");
-        return x / y ;
+        return x / y;
     }
 
 }
