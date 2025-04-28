3a04b501cf1b6979fc1224fba461e8547ad7c97a
dd-jiyun <dlwldbs8015@gmail.com>
2025-04-28 12:53:35 +0900

    1단계 - 사칙연산 계산기 구현

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+}
