666335aeb962940f8d2d7de749671d45a1477dde
boya-go <cbh0548@naver.com>
2025-04-28 23:10:11 +0900

    step1 - 계산기 기능 구현

---
+++ src/main/java/calculator.java
@@ -0,0 +1,18 @@
+public class calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
