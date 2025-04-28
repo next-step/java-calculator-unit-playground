d302d13b190ea54e610709f684187b6e3ecaa7ae
권장순 <jsoonworld@gmail.com>
2025-04-29 00:22:41 +0900

    feat(Number) : 숫자 값을 표현하는 VO(Value Object) 클래스 구현

---
+++ src/main/java/Number.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public class Number {
+
+    private final int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Number)) return false;
+        Number number = (Number) o;
+        return value == number.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
