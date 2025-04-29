31d7007f5f82af3b1fbdf1200e010a7cd5905191
kimYejin <kimphoby0125@gmail.com>
2025-04-29 21:56:11 +0900

    refact(StringCalculatorTest) : 테스트 메서드명을 given,when,then 구조로 리팩토링 및 single num에 대한 테스트케이스 추가

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import stringCalc.StringCalculator;
 
 import java.util.stream.Stream;
@@ -19,22 +20,23 @@ public class StringCalculatorTest {
 
         @Test
         @DisplayName("빈 문자열은 0을 반환한다.")
-        void emptyStringInputTest() {
+        void givenEmptyString_whenAdd_thenReturnsZero() {
             int actual = StringCalculator.add("");
             assertEquals(0, actual);
         }
 
         @Test
+        @ValueSource(strings = {"1","12","123"})
         @DisplayName("숫자 하나만 있을 경우, 그 숫자를 반환한다.")
-        void oneSizeStringInputTest() {
-            int actual = StringCalculator.add("1");
-            assertEquals(1, actual);
+        void givenSingleNum_whenAdd_thenReturnSingleNum(String value) {
+            int actual = StringCalculator.add(value);
+            assertEquals(Integer.parseInt(value), actual);
         }
 
         @ParameterizedTest
         @MethodSource("basicDelimeterStringInputArguments")
         @DisplayName("쉼표와 콜론으로 구분된 숫자들의 합을 반환한다.")
-        void basicDelimeterStringInputTest(String value, int expected) {
+        void givenBasicDelimiter_whenAdd_thenSumsCorrectly(String value, int expected) {
             int actual = StringCalculator.add(value);
             assertEquals(expected, actual);
         }
@@ -55,7 +57,7 @@ public class StringCalculatorTest {
         @ParameterizedTest
         @MethodSource("customDelimeterStringInputArguments")
         @DisplayName("커스텀 구분자를 인식하여 합을 반환한다.")
-        void customDelimeterStringInputTest(String value, int expected) {
+        void givenCustomDelimiter_whenAdd_thenSumsCorrectly(String value, int expected) {
             assertEquals(expected, StringCalculator.add(value));
         }
 
@@ -78,21 +80,21 @@ public class StringCalculatorTest {
         @ParameterizedTest
         @MethodSource("negativeIncludedStringInputArguments")
         @DisplayName("음수가 포함될 경우 RuntimeException이 발생한다.")
-        void negativeInputExceptionThrowTest(String value) {
+        void givenNegativeNum_whenAdd_thenThrowRuntimeException(String value) {
             assertThrows(RuntimeException.class, () -> StringCalculator.add(value));
         }
 
         @ParameterizedTest
         @MethodSource("noneAcceptedValueIncludedStringInputArguments")
         @DisplayName("숫자 이외의 값이 포함될 경우 RuntimeException이 발생한다.")
-        void noneAcceptedValueInputExceptionThrowTest(String value) {
+        void givenNonNumValue_whenAdd_thenThrowRuntimeException(String value) {
             assertThrows(RuntimeException.class, () -> StringCalculator.add(value));
         }
 
         @ParameterizedTest
         @MethodSource("spaceIncludedStringInputArguments")
         @DisplayName("공백이 포함될 경우 RuntimeException이 발생한다.")
-        void spaceIncludedInputExceptionThrowTest(String value) {
+        void givenSpace_whenAdd_thenThrowRuntimeException(String value) {
             assertThrows(RuntimeException.class, () -> StringCalculator.add(value));
         }
 
