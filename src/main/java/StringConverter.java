f21479c71e9554988a48939d151f9a73b9cddac4
dd-jiyun <dlwldbs8015@gmail.com>
2025-04-29 19:01:09 +0900

    fix: 변환 및 검증 메서드 명 수정

--- src/main/java/StringConverter.java
+++ src/main/java/StringConverter.java
@@ -3,20 +3,20 @@ import java.util.List;
 
 public class StringConverter {
 
-    public List<Integer> getNumbers(final String[] tokens) {
+    public List<Integer> convertAndValidateNumbers(final String[] tokens) {
         try {
             List<Integer> numbers = Arrays.stream(tokens)
                     .map(Integer::parseInt)
                     .toList();
 
-            checkNegativeNumber(numbers);
+            checkNegativeNumbers(numbers);
             return numbers;
         } catch (NumberFormatException e) {
             throw new RuntimeException("숫자가 아닌 다른 값이 입력되었습니다.");
         }
     }
 
-    private void checkNegativeNumber(final List<Integer> numbers) {
+    private void checkNegativeNumbers(final List<Integer> numbers) {
         for (int number : numbers) {
             if (number < 0) {
                 throw new RuntimeException("양의 정수만 입력할 수 있습니다.");
