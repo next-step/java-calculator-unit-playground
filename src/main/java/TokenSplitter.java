ed425336a197fdf10b99646894ecc5f54154883c
dd-jiyun <dlwldbs8015@gmail.com>
2025-04-29 18:02:47 +0900

    refactor: 문자열에서 커스텀 구분자를 나누는 기능

--- src/main/java/TokenSplitter.java
+++ src/main/java/TokenSplitter.java
@@ -1,8 +1,35 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TokenSplitter {
 
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String CUSTOM_SUFFIX = "\\n";
     private static final String DEFAULT_REGEX = "[,|:]";
+    private static final Pattern CUSTOM_REGEX = Pattern.compile("(?<=//)(.+?)(?=\\\\n)");
 
     public String[] getToken(final String input) {
+        if (input.startsWith(CUSTOM_PREFIX)) {
+            checkCustomFormat(input);
+            String preInput = preprocessInput(input);
+            return preInput.split(findCustom(input));
+        }
         return input.split(DEFAULT_REGEX);
+    }
+
+    private void checkCustomFormat(final String input) {
+        if (!input.contains(CUSTOM_SUFFIX)) {
+            throw new RuntimeException("커스텀 구분자 형식이 올바르지 않습니다.");
+        }
+    }
+
+    private String preprocessInput(final String input) {
+        return input.substring(input.indexOf(CUSTOM_SUFFIX) + CUSTOM_SUFFIX.length());
+    }
+
+    private String findCustom(final String input) {
+        Matcher matcher = CUSTOM_REGEX.matcher(input);
+        matcher.find();
+        return Pattern.quote(matcher.group());
     }
 }
