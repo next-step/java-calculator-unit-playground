69f9b567f072255c0c0b2c7ff41794c2ccd36790
dd-jiyun <dlwldbs8015@gmail.com>
2025-04-29 18:03:26 +0900

    test: 문자열에서 커스텀 구분자를 나누는 기능 테스트

--- src/test/java/TokenSplitterTest.java
+++ src/test/java/TokenSplitterTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,7 +10,7 @@ class TokenSplitterTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1:2:3", "1,2:3"})
-    @DisplayName("기본 구분자를 사용한 문자열을 입력했을 경우 정상적으로 나눈다.")
+    @DisplayName("기본 구분자를 사용한 문자열을 입력했을 경우 정상적으로 나누어진다.")
     void shouldRemove_whenBasicInput(String value) {
         // given
         TokenSplitter tokenSplitter = new TokenSplitter();
@@ -19,5 +20,35 @@ class TokenSplitterTest {
 
         // then
         assertEquals(Arrays.toString(token), "[1, 2, 3]");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\\n1;2;3"})
+    @DisplayName("커스텀 구분자를 사용한 문자열을 입력했을 경우 정상적으로 나누어진다.")
+    void shouldRemove_whenCustomInput(String customValue) {
+        // given
+        TokenSplitter tokenSplitter = new TokenSplitter();
+
+        // when
+        String[] token = tokenSplitter.getToken(customValue);
+
+        // then
+        assertEquals(Arrays.toString(token), "[1, 2, 3]");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 형식을 지키지 않았을 경우 예외가 발생한다.")
+    void shouldThrowException_whenInvalidCustomFormat() {
+        // given
+        String input = "//;1;2;3";
+        TokenSplitter tokenSplitter = new TokenSplitter();
+
+        // when
+        RuntimeException e = assertThrows(RuntimeException.class,
+                () -> tokenSplitter.getToken(input)
+        );
+
+        // then
+        assertEquals(e.getMessage(), "커스텀 구분자 형식이 올바르지 않습니다.");
     }
 }
