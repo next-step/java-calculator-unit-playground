3dea9a1d3cd83a509c354471a5774d7149c93598
dd-jiyun <dlwldbs8015@gmail.com>
2025-04-29 15:01:12 +0900

    test: 기본 구분자 계산 테스트
    - 기본 구분자를 사용한 문자열일 경우의 정상적인 합계를 반환하는지 테스트합니다.
    - 아무 값도 입력하지 않았을 경우의 0을 정상적으로 반환하는지 테스트합니다.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,89 +1,38 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorTest {
 
-    @Test
-    @DisplayName("두 개의 인자를 받아 정상적으로 덧셈 결과를 반환한다.")
-    void shouldReturnResult_Add() {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("입력 문자열이 없을 경우 0을 반환한다.")
+    void shouldReturnZero_whenNothingInputValue(String input) {
         // given
         Calculator calculator = new Calculator();
-        int a = 1;
-        int b = 2;
 
         // when
-        int expected = calculator.add(a, b);
+        int expected = calculator.sum(input);
 
-        // then
-        int actual = 3;
-        assertEquals(expected, actual);
+        //then
+        assertEquals(expected, 0);
     }
 
-    @Test
-    @DisplayName("두 개의 인자를 받아 정상적으로 뺄셈 결과를 반환한다.")
-    void shouldReturnResult_Subtraction() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1:2,3", "1,2,3"})
+    @DisplayName("입력한 문자열에 대해 올바른 덧셈 결과를 반환한다.")
+    void shouldReturnSum_whenInputString(String input) {
         // given
         Calculator calculator = new Calculator();
-        int a = 2;
-        int b = 1;
 
         // when
-        int expected = calculator.subtraction(a, b);
-
-        // then
-        int actual = 1;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("두 개의 인자를 받아 정상적으로 곱셈 결과를 반환한다.")
-    void shouldReturnResult_Multiplication() {
-        // given
-        Calculator calculator = new Calculator();
-        int a = 2;
-        int b = 3;
-
-        // when
-        int expected = calculator.multiplication(a, b);
+        int expected = calculator.sum(input);
 
         // then
         int actual = 6;
         assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("두 개의 인자를 받아 정상적으로 나눗셈 결과를 반환한다.")
-    void shouldReturnResult_Division() {
-        // given
-        Calculator calculator = new Calculator();
-        int a = 4;
-        int b = 2;
-
-        // when
-        int expected = calculator.division(a, b);
-
-        // then
-        int actual = 2;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("두 개의 인자 중 작은 값에서 큰 값을 나누려고 할 경우 예외가 발생한다.")
-    void shouldThrowException_whenIndivisibleValue() {
-        // given
-        Calculator calculator = new Calculator();
-        int a = 2;
-        int b = 4;
-
-        // when
-        ArithmeticException e = assertThrows(ArithmeticException.class,
-                () -> calculator.division(a, b)
-        );
-
-        // then
-        assertEquals(e.getMessage(), "작은 값으로 큰 값을 나눌 수 없습니다.");
     }
 }
