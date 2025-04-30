2d3e456bfc8a90b03674d5445ac1ef2dcce75785
권장순 <jsoonworld@gmail.com>
2025-04-30 15:34:20 +0900

    refactor(NumberParserTest) : AssertJ 적용

--- src/test/java/utils/NumberParserTest.java
+++ src/test/java/utils/NumberParserTest.java
@@ -4,8 +4,7 @@ import domain.Number;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("parser.NumberParser Test")
 class NumberParserTest {
@@ -14,12 +13,13 @@ class NumberParserTest {
     @DisplayName("정상 입력: 문자열을 정수로 변환하여 domain.Number 객체를 반환한다.")
     void parseValidInput() {
         Number number = NumberParser.parse("42");
-        assertEquals(new Number(42), number);
+        assertThat(number).isEqualTo(new Number(42));
     }
 
     @Test
     @DisplayName("비정상 입력: 숫자가 아닌 문자열 입력 시 예외를 발생시킨다.")
     void parseInvalidInput() {
-        assertThrows(IllegalArgumentException.class, () -> NumberParser.parse("abc"));
+        assertThatThrownBy(() -> NumberParser.parse("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
