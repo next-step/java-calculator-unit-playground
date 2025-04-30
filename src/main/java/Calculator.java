40263deb84812a669867104764d401177c1515ac
boya-go <cbh0548@naver.com>
2025-04-30 22:25:45 +0900

    refactor: 0으로 나누는 경우 예외처리

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -13,6 +13,9 @@ public class Calculator {
     }
 
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
         return a / b;
     }
 }
