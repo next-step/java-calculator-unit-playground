6194d53dec543ca529ae1fb5343389583d08f98e
권장순 <jsoonworld@gmail.com>
2025-04-30 15:28:08 +0900

    refactor(StringCalculatorTest) : AssertJ 적용

--- src/test/java/domain/StringCalculatorTest.java
+++ src/test/java/domain/StringCalculatorTest.java
@@ -1,7 +1,6 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,29 +12,26 @@ class StringCalculatorTest {
     @Test
     @DisplayName("빈 문자열 또는 null 입력 시 결과는 0이다")
     void returnZero_whenInputIsEmptyOrNull() {
-        // when & then
-        assertEquals(0, calculator.calculateSum(""));
-        assertEquals(0, calculator.calculateSum(null));
+        assertThat(calculator.calculateSum("")).isZero();
+        assertThat(calculator.calculateSum(null)).isZero();
     }
 
     @Test
     @DisplayName("쉼표 또는 콜론 구분자로 구분된 숫자를 합산한다")
     void sumNumbers_whenDefaultDelimiters() {
-        // when & then
-        assertEquals(6, calculator.calculateSum("1,2:3"));
+        assertThat(calculator.calculateSum("1,2:3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("커스텀 구분자가 지정된 경우 해당 구분자로 숫자를 합산한다")
     void sumNumbers_whenCustomDelimiter() {
-        // when & then
-        assertEquals(6, calculator.calculateSum("//;\n1;2;3"));
+        assertThat(calculator.calculateSum("//;\n1;2;3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("음수가 포함된 경우 예외를 던진다")
     void throwException_whenNegativeNumberExists() {
-        // when & then
-        assertThrows(IllegalArgumentException.class, () -> calculator.calculateSum("1,-2,3"));
+        assertThatThrownBy(() -> calculator.calculateSum("1,-2,3"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
