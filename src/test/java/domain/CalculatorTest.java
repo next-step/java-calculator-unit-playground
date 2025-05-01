7fa66ffcbb80cdc887e74f631e7f00ef445a0a1f
SanghyunLee <lgw9736@naver.com>
2025-05-01 17:12:32 +0900

    test: 문자열 입력 계산 테스트

--- src/test/java/domain/CalculatorTest.java
+++ src/test/java/domain/CalculatorTest.java
@@ -1,9 +1,13 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import domain.operatorImpl.AddOperator;
+import domain.operatorImpl.DivideOperator;
+import domain.operatorImpl.MultiplyOperator;
+import domain.operatorImpl.SubtractOperator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,78 +15,67 @@ import org.junit.jupiter.api.Test;
 @DisplayName("계산기 테스트")
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+    Calculator calculator = new Calculator(new Operator[]
+        {new AddOperator(), new SubtractOperator(), new MultiplyOperator(), new DivideOperator()});
 
     @Nested
-    @DisplayName("덧셈 테스트")
-    class AddTest {
+    @DisplayName("문자열 계산기 테스트")
+    class StringExpressionTest {
         @Test
-        @DisplayName("정수 덧셈 테스트")
-        void addIntegerTest() {
+        @DisplayName("문자열 입력 유효성 검증 테스트")
+        void validateInputExpressionTest() {
             // Given
 
             // When & Then
-            assertAll(
-                () -> assertEquals(5,calculator.add(1,4)),
-                () -> assertEquals(0,calculator.add(0,0)),
-                () -> assertEquals(-5,calculator.add(0,-5))
-            );
+            assertThrows(IllegalArgumentException.class, () -> calculator.calculateStringExpression("1"));
+            assertThrows(IllegalArgumentException.class, () -> calculator.calculateStringExpression("1+"));
+            assertThrows(IllegalArgumentException.class, () -> calculator.calculateStringExpression("1+2+3"));
+            assertDoesNotThrow(() -> calculator.calculateStringExpression("1+2"));
         }
-    }
-    
-    @Nested
-    @DisplayName("뺄셈 테스트")
-    class SubtractTest {
+
         @Test
-        @DisplayName("정수 뺄셈 테스트")
-        void subtractIntegerTest() {
+        @DisplayName("문자열 덧셈 테스트")
+        void calculateAddTest() {
             // Given
-            
+
             // When & Then
-            assertAll(
-                () -> assertEquals(3,calculator.subtract(4,1)),
-                () -> assertEquals(5,calculator.subtract(0,-5)),
-                () -> assertEquals(-5,calculator.subtract(0,5)),
-                () -> assertEquals(0,calculator.subtract(0,0))
-            );
+            assertEquals(5,calculator.calculateStringExpression("1+4"));
+            assertEquals(-3,calculator.calculateStringExpression("1+-4"));
+            assertEquals(-3,calculator.calculateStringExpression("1+ -4"));
         }
-    }
-    
-    @Nested
-    @DisplayName("곱셈 테스트")
-    class MultiplyTest {
+
         @Test
-        @DisplayName("정수 곱셈 테스트")
-        void multiplyIntegerTest() {
+        @DisplayName("문자열 뺄셈 테스트")
+        void calculateSubtractTest() {
             // Given
 
             // When & Then
-            assertAll(
-                () -> assertEquals(4,calculator.multiply(4,1)),
-                () -> assertEquals(0,calculator.multiply(0,3)),
-                () -> assertEquals(-6,calculator.multiply(3,-2)),
-                () -> assertEquals(0,calculator.multiply(0,0))
-            );
+            assertEquals(-3,calculator.calculateStringExpression("1-4"));
+            assertEquals(5,calculator.calculateStringExpression("1--4"));
+            assertEquals(5,calculator.calculateStringExpression("1- -4"));
         }
-    }
 
-    @Nested
-    @DisplayName("나눗셈 테스트")
-    class DivideTest {
         @Test
-        @DisplayName("정수 나눗셈 테스트")
-        void divideIntegerTest() {
+        @DisplayName("문자열 곱셈 테스트")
+        void calculateMultiplyTest() {
             // Given
 
             // When & Then
-            assertAll(
-                () -> assertEquals(4,calculator.divide(4,1)),
-                () -> assertEquals(2,calculator.divide(4,2)),
-                () -> assertEquals(-2,calculator.divide(4,-2)),
-                () -> assertEquals(0,calculator.divide(0,2))
-            );
+            assertEquals(4,calculator.calculateStringExpression("1*4"));
+            assertEquals(0,calculator.calculateStringExpression("1*0"));
+            assertEquals(-4,calculator.calculateStringExpression("1*-4"));
+        }
 
-            assertThrows(ArithmeticException.class, () -> calculator.divide(4,0));
+        @Test
+        @DisplayName("문자열 나눗셈 테스트")
+        void calculateDivideTest() {
+            // Given
+
+            // When & Then
+            assertEquals(0,calculator.calculateStringExpression("1/4"));
+            assertEquals(2,calculator.calculateStringExpression("4/2"));
+
+            assertThrows(ArithmeticException.class,() -> calculator.calculateStringExpression("1/0"));
         }
     }
 }
