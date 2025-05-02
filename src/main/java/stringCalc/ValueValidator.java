0c1811d2edc96b8b262460d54cf0c572caa63b66
kimYejin <kimphoby0125@gmail.com>
2025-05-02 17:55:10 +0900

    refact : 유효성 검증 메서드 분리

--- src/main/java/stringCalc/ValueValidator.java
+++ src/main/java/stringCalc/ValueValidator.java
@@ -6,18 +6,20 @@ public class ValueValidator {
 
     public static void validate(List<String> values) {
         for (String value : values) {
-            int num;
-            try {
-                num = Integer.parseInt(value);
-            } catch (NumberFormatException e) {
-                throw new RuntimeException("숫자가 아닌 값이 포함되었습니다. : " + value);
-            }
-            validateNegativeNum(num);
+            validateNotNumber(value);
+            validateNegativeNum(value);
         }
     }
 
-    private static void validateNegativeNum(int value) {
-        if (value < 0) {
+    private static void validateNotNumber(String value){
+        try{
+            Integer.parseInt(value);
+        }catch(NumberFormatException e){
+            throw new RuntimeException("숫자가 아닌 값이 포함되었습니다. : " + value);
+        }
+    }
+    private static void validateNegativeNum(String value) {
+        if (Integer.parseInt(value) < 0) {
             throw new RuntimeException("음수 값이 포함되어 있습니다. : " + value);
         }
     }
