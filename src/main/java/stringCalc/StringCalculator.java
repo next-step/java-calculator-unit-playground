785715333ffd5239b325318b53bbc65074119d68
kimYejin <kimphoby0125@gmail.com>
2025-05-04 16:26:37 +0900

    refact(StringCalculator): static import 제거하고 명시하여 가독성을 높임

--- src/main/java/stringCalc/StringCalculator.java
+++ src/main/java/stringCalc/StringCalculator.java
@@ -2,18 +2,16 @@ package stringCalc;
 
 import java.util.List;
 
-import static stringCalc.CustomDelimiterAndBodyExtractor.extract;
-import static stringCalc.StringParser.parse;
 
 public class StringCalculator {
     public static int add(String input) {
 
-        if (input == null || input.isEmpty() ){
+        if (input == null || input.isEmpty()){
             return 0;
         }
 
-        DelimiterAndBodyInfo info = extract(input);
-        List<String> values =  parse(info.getDelimiter(),info.getBody());
+        DelimiterAndBodyInfo info = CustomDelimiterAndBodyExtractor.extract(input);
+        List<String> values =  StringParser.parse(info.getDelimiter(),info.getBody());
 
         int sum = 0;
         for(String value : values){
