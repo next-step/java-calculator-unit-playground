1e04e35a1b75244c6156a32fa505ea8f21b1116e
boya-go <cbh0548@naver.com>
2025-05-07 00:13:38 +0900

    test: 접근제어자 수정에 따른 테스트 코드 수정

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,131 +1,14 @@
-import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-
 
 @DisplayName("문자열 계산기 테스트")
 public class StringCalculatorTest {
 
     StringCalculator stringCalculator = new StringCalculator();
-
-    @Nested
-    @DisplayName("문자열 계산기 입력 시 구분자를 반환한다.")
-    class DelimiterTest {
-
-        @Test
-        @DisplayName("기본 구분자 반환")
-        public void findDelimiter_ReturnDefaultDelimiter() {
-            SoftAssertions softly = new SoftAssertions();
-
-            softly.assertThat(stringCalculator.findDelimiter("1,4,5")).isEqualTo("[,|:]");
-            softly.assertThat(stringCalculator.findDelimiter("1:4:5")).isEqualTo("[,|:]");
-            softly.assertThat(stringCalculator.findDelimiter("1,4:5")).isEqualTo("[,|:]");
-
-            softly.assertAll();
-
-        }
-
-        @Test
-        @DisplayName("커스텀 구분자 반환")
-        public void findDelimiter_ReturnCustomDelimiter() {
-            SoftAssertions softly = new SoftAssertions();
-
-            softly.assertThat(stringCalculator.findDelimiter("//;\n142;34;3")).isEqualTo(";");
-            softly.assertThat(stringCalculator.findDelimiter("//!\n13!8!9!51!91")).isEqualTo("!");
-            softly.assertThat(stringCalculator.findDelimiter("//~\n22~34~21")).isEqualTo("~");
-
-            softly.assertAll();
-
-        }
-    }
-
-    @Nested
-    @DisplayName("구분자를 기준으로 입력된 문자열을 나눠 문자열 배열 형태로 반환한다.")
-    class ParseInputTest{
-
-        @Test
-        @DisplayName("기본 구분자 - 입력된 문자열 -> 문자열 배열 반환")
-        public void parseInput_DefaultDelimiter_ReturnStringArray() {
-
-            // Given
-            String input = "1,4,6:9";
-            String delimiter = "[,|:]";
-
-            // When
-            String[] actual = stringCalculator.parseInput(input, delimiter);
-
-            // then
-            String[] expected = new String[]{"1","4","6","9"};
-
-            assertThat(actual).isEqualTo(expected);
-        }
-
-        @Test
-        @DisplayName("커스텀 구분자 - 입력된 문자열 -> 문자열 배열 반환")
-        public void parseInput_CustomDelimiter_ReturnStringArray() {
-
-            // Given
-            String input = "//&\n10&8&9";
-            String delimiter = "&";
-
-            // When
-            String[] actual = stringCalculator.parseInput(input, delimiter);
-
-            // then
-            String[] expected = new String[]{"10","8","9"};
-
-            assertThat(actual).isEqualTo(expected);
-        }
-    }
-
-    @Nested
-    @DisplayName("문자열 배열을 숫자 리스트로 변환한다.")
-    class StringToNumberTest{
-
-        @Test
-        @DisplayName("숫자 리스트 형태로 반환")
-        public void makeNumberList_StringArray_ReturnIntegerList() {
-
-            // given
-            String[] tokens = {"3","55","23"};
-
-            // when
-            List<Integer> actual = stringCalculator.makeNumberList(tokens);
-
-            // then
-            List<Integer> expected = new ArrayList<>(Arrays.asList(3,55,23));
-
-            assertEquals(expected,actual);
-
-
-        }
-    }
-
-    @Nested
-    @DisplayName("만들어진 숫자리스트의 합을 출력한다.")
-    class AddNumbersTest{
-
-        @Test
-        @DisplayName("리스트의 합 반환")
-        public void addNumbers_ReturnSum() {
-
-            SoftAssertions softly = new SoftAssertions();
-
-            softly.assertThat(stringCalculator.addNumbers(Arrays.asList(1,5,3))).isEqualTo(9);
-            softly.assertThat(stringCalculator.addNumbers(Arrays.asList(33,22,11))).isEqualTo(66);
-
-        }
-    }
 
     @Nested
     @DisplayName("입력된 값에 따라 결과값을 출력한다.")
