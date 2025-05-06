bb6e2328aeacd1b680203103b6bad9cbf365485d
kimYejin <kimphoby0125@gmail.com>
2025-05-06 20:02:44 +0900

    refact(ValueValidator) : validateCondition에서 조건이 참이어야 정상인 것으로 이해되도록 수정

--- src/main/java/stringCalc/ValueValidator.java
+++ src/main/java/stringCalc/ValueValidator.java
@@ -6,19 +6,19 @@ public class ValueValidator {
 
     public static void validate(List<String> values) {
         for (String value : values) {
-            validateNotNumber(value);
-            validateNegativeNum(value);
+            validateIsNumber(value);
+            validateNonNegativeNum(value);
         }
     }
 
-    private static void validateNotNumber(String value){
+    private static void validateIsNumber(String value){
         try{
             Integer.parseInt(value);
         }catch(NumberFormatException e){
             throw new RuntimeException("숫자가 아닌 값이 포함되었습니다. : " + value);
         }
     }
-    private static void validateNegativeNum(String value) {
+    private static void validateNonNegativeNum(String value) {
         if (Integer.parseInt(value) < 0) {
             throw new RuntimeException("음수 값이 포함되어 있습니다. : " + value);
         }
