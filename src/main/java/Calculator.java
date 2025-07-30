934f58f0879904f8a947cd953adf1c159c4c9e16
asa9874 <pjkpjk1129@gmail.com>
2025-07-30 10:22:29 +0900

    feat: Calculator 클래스 계산 구현

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("0 나누기 오류");
+        }
+
+        return a / b;
+    }
+}
