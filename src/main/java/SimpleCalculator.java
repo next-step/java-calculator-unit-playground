c84322e22151ea42dcf204801c28ba9008f6e261
dldb-chamchi <dladkfl49@gmail.com>
2025-07-30 17:21:35 +0900

    feat: 예외 로직 추가

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,16 +1,24 @@
 public class SimpleCalculator {
 
-    public int add(int firstNum, int secondNum){
-        return firstNum + secondNum;
-    }
-    public int multiply(int firstNum, int secondNum){
-        return firstNum * secondNum;
-    }
-    public int divide(int firstNum, int secondNum){
-        return firstNum / secondNum;
-    }
-    public int minus(int firstNum, int secondNum){
-        return firstNum - secondNum;
+    public int add(int a, int b) {
+        return Math.addExact(a, b);
     }
 
+    public int minus(int a, int b) {
+        return Math.subtractExact(a, b);
+    }
+
+    public int multiply(int a, int b) {
+        return Math.multiplyExact(a, b);
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+        if (a == Integer.MIN_VALUE && b == -1) {
+            throw new ArithmeticException("정수 범위를 벗어났습니다.");
+        }
+        return a / b;
+    }
 }
