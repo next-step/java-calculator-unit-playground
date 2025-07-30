99257334cc7ba7b980bd3510a2c44fc69bcc8a45
dldb-chamchi <dladkfl49@gmail.com>
2025-07-30 17:46:08 +0900

    feat: 초간단 계산기 테스트 케이스 추가

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,31 +11,49 @@ public class SimpleCalculatorTest {
     class SimpleTest {
 
         @Test
-        void addTest(){
+        void addTest() {
             SimpleCalculator calc = new SimpleCalculator();
             assertEquals(3, calc.add(1, 2));
             assertEquals(5, calc.add(3, 2));
         }
 
         @Test
-        void minusTest(){
+        void minusTest() {
             SimpleCalculator calc = new SimpleCalculator();
             assertEquals(3, calc.minus(5, 2));
             assertEquals(-2, calc.minus(3, 5));
         }
 
         @Test
-        void multiply(){
+        void multiply() {
             SimpleCalculator calc = new SimpleCalculator();
             assertEquals(10, calc.multiply(5, 2));
             assertEquals(15, calc.multiply(3, 5));
         }
 
         @Test
-        void divide(){
+        void divide() {
             SimpleCalculator calc = new SimpleCalculator();
             assertEquals(5, calc.divide(10, 2));
             assertEquals(30, calc.divide(180, 6));
+        }
+
+        @Test
+        void errorTestDivideZero() {
+            SimpleCalculator calc = new SimpleCalculator();
+            assertThatThrownBy(() -> calc.divide(2, 0)).isInstanceOf(ArithmeticException.class);
+        }
+
+        @Test
+        void errorTestOverFlowMinus() {
+            SimpleCalculator calc = new SimpleCalculator();
+            assertThatThrownBy(() -> calc.minus(Integer.MAX_VALUE, -3)).isInstanceOf(ArithmeticException.class);
+        }
+
+        @Test
+        void errorTestOverFlowMultiply() {
+            SimpleCalculator calc = new SimpleCalculator();
+            assertThatThrownBy(() -> calc.multiply(Integer.MAX_VALUE, 3)).isInstanceOf(ArithmeticException.class);
         }
     }
 
