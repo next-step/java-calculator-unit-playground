60207fc534ef5e5a82156590e98c1d6caf143844
dldb-chamchi <dladkfl49@gmail.com>
2025-07-30 18:55:02 +0900

    feat: 문자열 계산기 테스트 생성

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+    @Nested
+    class StringTest{
+        @Test
+        void defaultDelimiterTest(){
+            StringCalculator calc = new StringCalculator();
+            assertEquals(3, calc.calculate("1,2"));
+            assertEquals(6, calc.calculate("1,2,3"));
+            assertEquals(3, calc.calculate("1:2"));
+            assertEquals(38, calc.calculate("1:2,3:4,5:6:7:10"));
+        }
+
+        @Test
+        void customDelimiterTest(){
+            StringCalculator calc = new StringCalculator();
+            assertEquals(6, calc.calculate("//;\n1;2;3"));
+            assertEquals(5, calc.calculate("//;;\n1;;1;;2;;1"));
+            assertEquals(6, calc.calculate("//.\n1.2.3"));
+            assertEquals(3, calc.calculate("//|\n1|2"));
+            assertEquals(101, calc.calculate("//?\n10?11?12?13?14?20?21"));
+        }
+
+        @Test
+        void exceptionTest(){
+            StringCalculator calc = new StringCalculator();
+
+            assertThatThrownBy(() -> calc.calculate("-1,2")).isInstanceOf(RuntimeException.class);
+            assertThatThrownBy(() -> calc.calculate("2147483647,10")).isInstanceOf(RuntimeException.class);
+        }
+
+        @Test
+        void blankTest(){
+            StringCalculator calc = new StringCalculator();
+            assertEquals(0, calc.calculate(""));
+            assertThatThrownBy(() -> calc.calculate(" ")).isInstanceOf(RuntimeException.class);
+        }
+    }
+}
