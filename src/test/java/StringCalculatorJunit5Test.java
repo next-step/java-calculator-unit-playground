800bd3ed6b97120a996ca14b9613627893d145c5
asa9874 <pjkpjk1129@gmail.com>
2025-07-30 13:05:29 +0900

    feat: StringCalculatorJunit5Test 테스트 생성

---
+++ src/test/java/StringCalculatorJunit5Test.java
@@ -0,0 +1,54 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorJunit5Test {
+    private StringCalculator stringCalculator = new StringCalculator();
+
+    @Nested
+    class AddTest {
+        @Test
+        void 빈_문자열() {
+            assertEquals(0, stringCalculator.add(""));
+        }
+
+        @Test
+        void 하나의_숫자() {
+            assertEquals(1, stringCalculator.add("1"));
+            assertEquals(5, stringCalculator.add("5"));
+        }
+
+        @Test
+        void 두_숫자_더하기() {
+            assertEquals(3, stringCalculator.add("1,2"));
+            assertEquals(7, stringCalculator.add("3,4"));
+        }
+
+        @Test
+        void 여러_숫자_더하기() {
+            assertEquals(6, stringCalculator.add("1,2,3"));
+            assertEquals(15, stringCalculator.add("4,5,6"));
+        }
+
+        @Test
+        void 커스텀_구분자() {
+            assertEquals(3, stringCalculator.add("//;\n1;2"));
+            assertEquals(12, stringCalculator.add("//;\n4;5,1:2"));
+        }
+
+        @Test
+        void 음수_예외() {
+            Exception exception = assertThrows(RuntimeException.class, () -> {
+                stringCalculator.add("-1");
+            });
+        }
+
+        @Test
+        void 숫자가_아닌_예외() {
+            Exception exception = assertThrows(RuntimeException.class, () -> {
+                stringCalculator.add("1,a");
+            });
+        }
+    }
+}
