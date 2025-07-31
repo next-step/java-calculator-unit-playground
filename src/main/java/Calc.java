14f2ea2b701cef3ce4094b03d7a7f4b872774278
박태진 <140797244+taejinn@users.noreply.github.com>
2025-07-31 18:52:24 +0900

    feat: 계산 기능 추가

---
+++ src/main/java/Calc.java
@@ -0,0 +1,24 @@
+public class Calc {
+    public int add(int a, int b) {
+        int res = a + b;
+        return res;
+    }
+
+    public int minus(int a, int b) {
+        int res = a - b;
+        return res;
+    }
+
+    public int times(int a, int b) {
+        int res = a * b;
+        return res;
+    }
+
+    public int divide(int a, int b) {
+        if (a == 0 || b == 0) {
+            throw new IllegalArgumentException("0 으로 나눌 수 없습니다.");
+        }
+        int res = a / b;
+        return res;
+    }
+}
