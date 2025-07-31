bc1e5b68f075b5b22fbb84e752fecd47c120d0f9
dldb-chamchi <dladkfl49@gmail.com>
2025-07-31 15:18:18 +0900

    fix: 컨벤션 수정

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -7,52 +7,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleCalculatorTest {
+
+    private final SimpleCalculator calc = new SimpleCalculator();
+
     @Nested
     class SimpleTest {
 
         @Test
         void addTest() {
-            SimpleCalculator calc = new SimpleCalculator();
             assertEquals(3, calc.add(1, 2));
             assertEquals(5, calc.add(3, 2));
         }
 
         @Test
         void minusTest() {
-            SimpleCalculator calc = new SimpleCalculator();
             assertEquals(3, calc.minus(5, 2));
             assertEquals(-2, calc.minus(3, 5));
         }
 
         @Test
         void multiply() {
-            SimpleCalculator calc = new SimpleCalculator();
             assertEquals(10, calc.multiply(5, 2));
             assertEquals(15, calc.multiply(3, 5));
         }
 
         @Test
         void divide() {
-            SimpleCalculator calc = new SimpleCalculator();
             assertEquals(5, calc.divide(10, 2));
             assertEquals(30, calc.divide(180, 6));
         }
 
         @Test
         void errorTestDivideZero() {
-            SimpleCalculator calc = new SimpleCalculator();
             assertThatThrownBy(() -> calc.divide(2, 0)).isInstanceOf(ArithmeticException.class);
         }
 
         @Test
         void errorTestOverFlowMinus() {
-            SimpleCalculator calc = new SimpleCalculator();
             assertThatThrownBy(() -> calc.minus(Integer.MAX_VALUE, -3)).isInstanceOf(ArithmeticException.class);
         }
 
         @Test
         void errorTestOverFlowMultiply() {
-            SimpleCalculator calc = new SimpleCalculator();
             assertThatThrownBy(() -> calc.multiply(Integer.MAX_VALUE, 3)).isInstanceOf(ArithmeticException.class);
         }
     }
@@ -61,7 +57,6 @@ public class SimpleCalculatorTest {
     class CalculatorTest{
         @Test
         void simpleCalcTest(){
-            SimpleCalculator calc = new SimpleCalculator();
             assertThat(calc.add(1, 2)).isEqualTo(3);
             assertThat(calc.minus(1,2)).isEqualTo(-1);
             assertThat(calc.divide(4, 2)).isEqualTo(2);
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -5,11 +5,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+
+    private final StringCalculator calc = new StringCalculator();
+
     @Nested
     class StringTest{
+
         @Test
         void defaultDelimiterTest(){
-            StringCalculator calc = new StringCalculator();
             assertEquals(3, calc.calculate("1,2"));
             assertEquals(6, calc.calculate("1,2,3"));
             assertEquals(3, calc.calculate("1:2"));
@@ -18,7 +21,6 @@ public class StringCalculatorTest {
 
         @Test
         void customDelimiterTest(){
-            StringCalculator calc = new StringCalculator();
             assertEquals(6, calc.calculate("//;\n1;2;3"));
             assertEquals(5, calc.calculate("//;;\n1;;1;;2;;1"));
             assertEquals(6, calc.calculate("//.\n1.2.3"));
@@ -28,15 +30,13 @@ public class StringCalculatorTest {
 
         @Test
         void exceptionTest(){
-            StringCalculator calc = new StringCalculator();
-
             assertThatThrownBy(() -> calc.calculate("-1,2")).isInstanceOf(RuntimeException.class);
             assertThatThrownBy(() -> calc.calculate("2147483647,10")).isInstanceOf(RuntimeException.class);
+            assertThatThrownBy(() -> calc.calculate("hello")).isInstanceOf(RuntimeException.class);
         }
 
         @Test
         void blankTest(){
-            StringCalculator calc = new StringCalculator();
             assertEquals(0, calc.calculate(""));
             assertThatThrownBy(() -> calc.calculate(" ")).isInstanceOf(RuntimeException.class);
         }
