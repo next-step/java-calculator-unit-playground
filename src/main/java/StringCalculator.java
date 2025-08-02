06bbd9f65ff8f28d06593e52240af7a43fa85a9c
dldb-chamchi <dladkfl49@gmail.com>
2025-08-02 11:05:35 +0900

    fix: 빈칸 입력은 바로 return 0 하도록 수정

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,49 +2,50 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public String validateInput(String input){
-        if(input == null) throw new RuntimeException("null은 허용되지 않습니다.");
-        return input;
+    private void validateInput(String input){
+        if (input == null){
+            throw new RuntimeException("null은 허용되지 않습니다.");
+        }
     }
 
-    public int getDelimiter(String input) {
-        if(input.isEmpty()) return -2; //문자열이 빈 경우
-
+    private int getDelimiterIndex(String input) {
         if (input.startsWith("//")) {
             int delimiterIdx = input.indexOf("\n");
-            if (delimiterIdx == -1) throw new RuntimeException("잘못된 커스텀 구분자 형식입니다.");
+            if (delimiterIdx == -1){
+                throw new RuntimeException("잘못된 커스텀 구분자 형식입니다.");
+            }
             return delimiterIdx;
         }
-
         return -1; //기본 구분자대로 문자열을 나눔
     }
 
-    private String[] splitInput(String checkedInput, int idx){
-        if(idx == -1){
+    private String[] splitInput(String checkedInput, int delimiterIdx){
+        if (delimiterIdx == -1){
             String delimiter = "[,:]";
             return checkedInput.split(delimiter);
         }
-        else if(idx == -2){
-            return new String[0]; //빈 문자열인 경우
-        }
         else{
-            String delimiter = checkedInput.substring(2, idx);
+            String delimiter = checkedInput.substring(2, delimiterIdx);
             String regex = Pattern.quote(delimiter);
-            return checkedInput.substring(idx + 1).split(regex);
+            return checkedInput.substring(delimiterIdx + 1).split(regex);
         }
     }
 
-    public int sum(String[] strings){
-        if(strings.length == 0) return 0;
+    private int sum(String[] strings){
+        if (strings.length == 0){
+            return 0;
+        }
 
         int sum = 0;
 
         for(var n : strings){
-            if(n.isEmpty()) throw new RuntimeException("빈 문자열입니다.");
+            if (n.isEmpty()){
+                throw new RuntimeException("빈 문자열입니다.");
+            }
 
             int num = validateNumbers(n);
 
-            if(sum > Integer.MAX_VALUE - num || sum < Integer.MIN_VALUE + num) throw new RuntimeException("int 범위를 벗어났습니다.");
+            if (sum > Integer.MAX_VALUE - num || sum < Integer.MIN_VALUE + num) throw new RuntimeException("int 범위를 벗어났습니다.");
 
             sum += num;
         }
@@ -55,7 +56,9 @@ public class StringCalculator {
         int num;
         try{
             num = Integer.parseInt(n);
-            if(num < 0) throw new RuntimeException("음수는 입력이 불가합니다.");
+            if (num < 0){
+                throw new RuntimeException("음수는 입력이 불가합니다.");
+            }
         }
         catch(NumberFormatException msg){
             throw new RuntimeException("int 값을 벗어났거나 잘못된 숫자 형식입니다.");
@@ -64,9 +67,12 @@ public class StringCalculator {
     }
 
     public int calculate(String input){
-        String checkedInput = validateInput(input);
-        int idx = getDelimiter(checkedInput);
-        String[] strings = splitInput(checkedInput, idx);
+        validateInput(input);
+        if (input.isEmpty()){
+            return 0;
+        }
+        int delimiterIndex = getDelimiterIndex(input);
+        String[] strings = splitInput(input, delimiterIndex);
         return sum(strings);
     }
 }
