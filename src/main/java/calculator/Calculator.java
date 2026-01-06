337100ec8f04b335797fc00edb05e6194cee7420
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-06 21:32:24 +0900

    feat: 계산기 클래스에 사칙연산에 대한 메서드 작성, 나눗셈의 경우 0으로 나누면 예외처리

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+        return a / b;
+    }
+}
