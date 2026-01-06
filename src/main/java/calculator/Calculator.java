dc63af1dffca5f34465510d9279278a33c72eafb
Gopistol <rhrjs0131@naver.com>
2026-01-06 17:09:27 +0900

    feat: 사칙연산 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int minus(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+        }
+        return a / b;
+    }
+}
