32ade1a94555aa20712bd7da2e8e6d48149779ea
HeeJoon-Kwon <2390kwon@g.skku.edu>
2026-03-21 11:08:30 +0900

    feat: level.4 calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -16,4 +16,40 @@ public class Calculator {
     int divide(int num1, int num2) {
         return num1 / num2;
     }
+
+    int stringAdd(String expression) {
+
+        if (expression.equals("")) {
+            return 0;
+        }
+
+
+        if (expression.matches("//.\n.*")) {
+            String delimiter = expression.split("//|\n")[1];
+            String withoutDelimiter = expression.split("\n")[1];
+            String[] tokens = withoutDelimiter.split(delimiter);
+
+            int sum = 0;
+            for (String token : tokens) {
+                if (token.matches("\\d")) {
+                    sum += Integer.parseInt(token);
+                } else {
+                    throw new RuntimeException();
+                }
+            }
+            return sum;
+        } else {
+            String[] tokens = expression.split("[,:]", -1);
+            int sum = 0;
+            for (String token : tokens) {
+                if (token.matches("\\d")) {
+                    sum += Integer.parseInt(token);
+                } else {
+                    throw new RuntimeException();
+                }
+            }
+
+            return sum;
+        }
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,7 +3,8 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorTest {
 
@@ -18,7 +19,7 @@ class CalculatorTest {
 
 
         // then
-        assertEquals(9, actual);
+        assertThat(actual).isEqualTo(9);
     }
 
     @DisplayName("두 개의 정수를 뺀 결과를 반환할 수 있다.")
@@ -32,7 +33,7 @@ class CalculatorTest {
 
 
         // then
-        assertEquals(3, actual);
+        assertThat(actual).isEqualTo(3);
     }
 
     @DisplayName("두 개의 정수를 곱한 결과를 반환할 수 있다.")
@@ -46,7 +47,7 @@ class CalculatorTest {
 
 
         // then
-        assertEquals(18, actual);
+        assertThat(actual).isEqualTo(18);
     }
 
     @DisplayName("두 개의 정수를 나눈 결과를 반환할 수 있다.")
@@ -60,7 +61,47 @@ class CalculatorTest {
 
 
         // then
-        assertEquals(2, actual);
+        assertThat(actual).isEqualTo(2);
+    }
+
+    @DisplayName("문자열을 분리해 더할 수 있다")
+    @Test
+    void testStringAdd(){
+        Calculator calculator = new Calculator();
+
+        int result = calculator.stringAdd("");
+        // assertEquals(0, result);
+        assertThat(result).isEqualTo(0);
+
+        result = calculator.stringAdd("2,3");
+        // assertEquals(5, result);
+        assertThat(result).isEqualTo(5);
+
+        result = calculator.stringAdd("2,3,5");
+        // assertEquals(10, result);
+        assertThat(result).isEqualTo(10);
+
+        result = calculator.stringAdd("4:5");
+        // assertEquals(9, result);
+        assertThat(result).isEqualTo(9);
+
+        result = calculator.stringAdd("4:5:2");
+        // assertEquals(11, result);
+        assertThat(result).isEqualTo(11);
+
+        result = calculator.stringAdd("//;\n1;2;3");
+        // assertEquals(6, result);
+        assertThat(result).isEqualTo(6);
+
+        result = calculator.stringAdd("//-\n1-2-3-4");
+        // assertEquals(10, result);
+        assertThat(result).isEqualTo(10);
+
+        // assertThrows(RuntimeException.class, () -> calculator.stringAdd("//:\n-1:2:3:4"));
+        assertThatThrownBy(() -> calculator.stringAdd("//:\n-1:2:3:4")).isInstanceOf(RuntimeException.class);
+
+        // assertThrows(RuntimeException.class, () -> calculator.stringAdd("//:\n1:a:3:4"));
+        assertThatThrownBy(() -> calculator.stringAdd("//:\n1:a:3:4")).isInstanceOf(RuntimeException.class);
     }
 
 }
